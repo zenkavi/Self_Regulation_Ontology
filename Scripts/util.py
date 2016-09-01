a4b8ff92ab869b7d4ef3db440973f6f188ea9c2c
ian <ianeisenberg90@gmail.com>
2016-09-01 12:50:12 -0700

    moved discovery sample script

--- Scripts/util.py
+++ Scripts/util.py
@@ -52,13 +52,7 @@ def set_discovery_sample(n, discovery_n, seed = None):
     np.random.shuffle(sample)
     return sample
 
-seed = 1960
-n = 500
-discovery_n = 200
-subjects = ['s' + str(i).zfill(3) for i in range(1,n+1)]
-subject_order = set_discovery_sample(n, discovery_n, seed)
-subject_assignment_df = pd.DataFrame({'dataset': subject_order}, index = subjects)
-subject_assignment_df.to_csv('../subject_assignment.csv')
+
 
 def anonymize_data(data):
     complete_workers = (data.groupby('worker_id').count().finishtime>=63)
