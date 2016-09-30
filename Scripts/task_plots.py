680124e989a8d8029e12e78c53bb8c402a52310b
Ian <IanEisenberg90@gmail.com>
2016-09-29 19:56:30 -0700

    updates

--- Scripts/task_plots.py
+++ Scripts/task_plots.py
@@ -1,9 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 21 08:58:43 2016
+from os import path
+import numpy as np
+import pandas as pd
+from util import get_info
+import seaborn as sns
 
-@author: ian
-"""
+#load Data
+data_dir=path.join(get_info('base_directory'),'Data/Discovery_9-26-16')
+
+# get DV df
+DV_df = pd.read_csv(path.join(data_dir,'meaningful_variables.csv'))
+
+tasks = np.unique(DV_df.columns.map(lambda x: x.split('.')[0]))
+for task in 
+p = sns.pairplot(subset, diag_kws = {'bins': 50})
+p.savefig('Plots/pair_plot_all_variables.pdf', dpi = 300)
+
+
+
+
 plt.figure(figsize=(12,8))
 data.groupby('worker_id').passed_QC.sum().hist(bins = 50)
 
---
+++ discovery_graph_analyses/util.py
@@ -0,0 +1,76 @@
+#***************************************************
+# ********* Plotting Functions **********************
+#**************************************************
+def dendroheatmap(df, labels = True):
+    """
+    :df: plot hierarchical clustering and heatmap
+    """
+    #clustering
+    
+    row_clusters = linkage(df.values, method='complete', metric='euclidean')
+    pd.DataFrame(row_clusters, 
+                 columns=['row label 1', 'row label 2', 'distance', 'no. of items in clust.'],
+                 index=['cluster %d' %(i+1) for i in range(row_clusters.shape[0])])
+    
+    #dendrogram
+    row_dendr = dendrogram(row_clusters, labels=df.columns, no_plot = True)
+    df_rowclust = df.ix[row_dendr['leaves'],row_dendr['leaves']]
+    sns.set_style("white")
+    fig = plt.figure(figsize = [16,16])
+    ax = fig.add_axes([.1,.2,.6,.6]) 
+    cax = fig.add_axes([0.02,0.3,0.02,0.4]) 
+    sns.heatmap(df_rowclust, ax = ax, cbar_ax = cax, xticklabels = False)
+    ax.yaxis.tick_right()
+    ax.set_yticklabels(df_rowclust.columns[::-1], rotation=0, rotation_mode="anchor", fontsize = 'small', visible = labels)
+    ax.set_xticklabels(df_rowclust.columns, rotation=-90, rotation_mode = "anchor", ha = 'left')
+    ax1 = fig.add_axes([.1,.8,.6,.2])
+    plt.axis('off')
+    row_dendr = dendrogram(row_clusters, orientation='top',  
+                           count_sort='ascending', ax = ax1) 
+    return fig
+
+def dendroheatmap_left(df, labels = True):
+    """
+    :df: plot hierarchical clustering and heatmap, dendrogram on left
+    """
+    #clustering
+    
+    row_clusters = linkage(df.values, method='complete', metric='euclidean')
+    pd.DataFrame(row_clusters, 
+                 columns=['row label 1', 'row label 2', 'distance', 'no. of items in clust.'],
+                 index=['cluster %d' %(i+1) for i in range(row_clusters.shape[0])])
+    
+    #dendrogram
+    row_dendr = dendrogram(row_clusters, labels=df.columns, no_plot = True)
+    df_rowclust = df.ix[row_dendr['leaves'],row_dendr['leaves']]
+    sns.set_style("white")
+    fig = plt.figure(figsize = [16,16])
+    ax = fig.add_axes([.16,.3,.62,.62]) 
+    cax = fig.add_axes([0.21,0.25,0.5,0.02]) 
+    sns.heatmap(df_rowclust, ax = ax, cbar_ax = cax, cbar_kws = {'orientation': 'horizontal'}, xticklabels = False)
+    ax.yaxis.tick_right()
+    ax.set_yticklabels(df_rowclust.columns[::-1], rotation=0, rotation_mode="anchor", fontsize = 'large', visible = labels)
+    ax.set_xticklabels(df_rowclust.columns, rotation=-90, rotation_mode = "anchor", ha = 'left')
+    ax1 = fig.add_axes([.01,.3,.15,.62])
+    plt.axis('off')
+    row_dendr = dendrogram(row_clusters, orientation='right',  
+                           count_sort='descending', ax = ax1) 
+    return fig
+    
+def heatmap(df):
+    """
+    :df: plot heatmap
+    """
+    plt.Figure(figsize = [16,16])
+    sns.set_style("white")
+    fig = plt.figure(figsize = [12,12])
+    ax = fig.add_axes([.1,.2,.6,.6]) 
+    cax = fig.add_axes([0.02,0.3,0.02,0.4]) 
+    sns.heatmap(df, ax = ax, cbar_ax = cax, xticklabels = False)
+    ax.yaxis.tick_right()
+    ax.set_yticklabels(df.columns[::-1], rotation=0, rotation_mode="anchor", fontsize = 'large')
+    ax.set_xticklabels(df.columns, rotation=-90, rotation_mode = "anchor", ha = 'left') 
+    return fig
+    
+def zscore_data(data):
+    return (data - data.mean())/data.std()
