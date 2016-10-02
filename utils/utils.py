89f932f5a32b3f22f0410cd06e7503c66eec085a
poldrack <poldrack@gmail.com>
2016-10-02 09:31:23 -0700

    added functions to load data

--- utils/utils.py
+++ utils/utils.py
@@ -2,13 +2,14 @@
 some util functions
 """
 
-import os
+import os,json
+import pandas
 
 def get_info(item,infile='../Self_Regulation_Settings.txt'):
     """
     get info from settings file
     """
-    
+
     infodict={}
     try:
         assert os.path.exists(infile)
@@ -26,3 +27,35 @@ def get_info(item,infile='../Self_Regulation_Settings.txt'):
         raise Exception('infodict does not include requested item')
     return infodict[item]
 
+def get_survey_data(dataset):
+    basedir=get_info('base_directory')
+    infile=os.path.join(basedir,'data/Derived_Data/%s/surveydata.csv'%dataset)
+    surveydata=pandas.read_csv(infile,index_col=0)
+    keyfile=os.path.join(basedir,'data/Derived_Data/%s/surveyitem_key.txt'%dataset)
+    with open(keyfile) as f:
+        keylines=[i.strip().split('\t') for i in f.readlines()]
+    surveykey={}
+    for k in keylines:
+        surveykey[k[0]]=k[2]
+    return surveydata,surveykey
+
+def load_metadata(variable,basedir):
+
+    with open(os.path.join(basedir,'%s.json'%variable)) as outfile:
+            metadata=json.load(outfile)
+    return metadata
+
+def get_single_dataset(dataset,survey):
+    basedir=get_info('base_directory')
+    infile=os.path.join(basedir,'data/Derived_Data/%s/surveydata/%s.tsv'%(dataset,survey))
+    print(infile)
+    assert os.path.exists(infile)
+    if survey.find('ordinal')>-1:
+        survey=survey.replace('_ordinal','')
+    mdfile=os.path.join(basedir,'data/Derived_Data/%s/metadata/%s.json'%(dataset,survey))
+    print(mdfile)
+    assert os.path.exists(mdfile)
+    data=pandas.read_csv(infile,index_col=0,sep='\t')
+    metadata=load_metadata(survey,os.path.join(basedir,
+        'data/Derived_Data/%s/metadata'%dataset))
+    return data,metadata
