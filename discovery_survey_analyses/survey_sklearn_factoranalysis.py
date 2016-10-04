40606f552d5377a652e8c10eef919230d8df4f55
poldrack <poldrack@gmail.com>
2016-10-04 08:31:08 -0700

    updated to latest data org

--- discovery_survey_analyses/survey_sklearn_factoranalysis.py
+++ discovery_survey_analyses/survey_sklearn_factoranalysis.py
@@ -3,7 +3,7 @@ based loosely on PyIBP/example/example.py
 using full LA2K dataset
 """
 
-import os,glob
+import os,glob,sys
 import numpy,pandas
 import json
 
@@ -11,20 +11,23 @@ from sklearn.preprocessing import scale
 from sklearn.decomposition import FactorAnalysis
 from sklearn import cross_validation
 
-data=pandas.read_csv('surveydata.csv',index_col='worker')
-with open('all_survey_metadata.json', encoding='utf-8') as data_file:
-        md = json.loads(data_file.read())
-cdata=data.values
+# this is kludgey but it works
+sys.path.append('../utils')
+from utils import get_info,get_survey_data
 
-all_metadata={}
-for measure in md.keys():
-    for k in md[measure].keys():
-        all_metadata[k]=md[measure][k]
+dataset='Discovery_9-26-16'
+basedir=get_info('base_directory')
+derived_dir=os.path.join(basedir,'Data/Derived_Data/%s'%dataset)
+
+
+data,surveykey=get_survey_data(dataset)
+
+cdata=data.values
 
 
 kf = cross_validation.KFold(cdata.shape[0], n_folds=4)
 
-max_components=20
+max_components=30
 
 sc=numpy.zeros((max_components,4))
 
@@ -34,11 +37,11 @@ for n_components in range(1,max_components):
     for train,test in kf:
         train_data=cdata[train,:]
         test_data=cdata[test,:]
-    
+
         fa.fit(train_data)
         sc[n_components,fold]=fa.score(test_data)
         fold+=1
-        
+
 meanscore=numpy.mean(sc,1)
 meanscore[0]=-numpy.inf
 maxscore=numpy.argmax(meanscore)
@@ -53,14 +56,6 @@ for c in range(maxscore):
     print('')
     print('component %d'%c)
     for i in range(3):
-        print('%f: %s %s'%(fa.components_[c,s[i]],data.columns[s[i]],all_metadata[data.columns[s[i]]]['Description']))
+        print('%f: %s %s'%(fa.components_[c,s[i]],data.columns[s[i]],surveykey[data.columns[s[i]]]))
     for i in range(len(s)-4,len(s)-1):
-        print('%f: %s %s'%(fa.components_[c,s[i]],data.columns[s[i]],all_metadata[data.columns[s[i]]]['Description']))
-    
-with open('variable_key.txt','w') as f:   
-    for k in all_metadata.keys():
-        if 'Derivative' in all_metadata[k]:
-            continue
-        f.write('%s\t%s\n'%(k,all_metadata[k]['Description'].replace('"','')))
-    
-
+        print('%f: %s %s'%(fa.components_[c,s[i]],data.columns[s[i]],surveykey[data.columns[s[i]]]))
