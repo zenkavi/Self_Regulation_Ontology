5efcaebbc434288a350f02586a5f6b4fbd96472a
poldrack <poldrack@gmail.com>
2016-10-08 15:24:11 -0700

    added imputation and dimensionality reduction into pipeline

--- discovery_prediction_analyses/demographic_feature_importance_behav.py
+++ discovery_prediction_analyses/demographic_feature_importance_behav.py
@@ -12,10 +12,11 @@ import warnings
 import numpy,pandas
 from sklearn.svm import LinearSVC,SVC,OneClassSVM
 from sklearn.linear_model import LinearRegression,LogisticRegressionCV,RandomizedLogisticRegression,ElasticNet,ElasticNetCV,Ridge,RidgeCV
-from sklearn.preprocessing import scale,StandardScaler
+from sklearn.preprocessing import scale,StandardScaler,FunctionTransformer
 from sklearn.decomposition import FactorAnalysis,PCA
 from sklearn.metrics import accuracy_score,f1_score,roc_auc_score,classification_report,confusion_matrix
 from sklearn.model_selection import GridSearchCV,StratifiedShuffleSplit,cross_val_score
+from sklearn.pipeline import make_pipeline
 from imblearn.over_sampling import SMOTE
 from imblearn.combine import SMOTETomek
 import fancyimpute
@@ -76,13 +77,10 @@ if shuffle:
 else:
     shuffletag=''
 
+def softimpute(X, y=None):
+    return(fancyimpute.SoftImpute(verbose=False).complete(X))
 
-class SoftImpute:
-    def fit:
-        pass
-
-    def fit_transform(data):
-        return fancyimpute.SoftImpute().complete(data)
+imputer = FunctionTransformer(softimpute, validate=False)
 
 # for some items, we want to use somethign other than the minimum as the
 # cutoff:
@@ -94,9 +92,12 @@ item_thresholds={'Nervous':1,
                 'Worthless':1}
 
 def get_demog_data(binary_vars=binary_vars,ordinal_vars=[],item_thresholds={},binarize=True):
-    demogdata=pandas.read_csv(os.path.join(derived_dir,'surveydata/demographics.tsv'),index_col=0,delimiter='\t')
-    healthdata=pandas.read_csv(os.path.join(derived_dir,'surveydata/health_ordinal.tsv'),index_col=0,delimiter='\t')
-    alcdrugs=pandas.read_csv(os.path.join(derived_dir,'surveydata/alcohol_drugs_ordinal.tsv'),index_col=0,delimiter='\t')
+    demogdata=pandas.read_csv(os.path.join(derived_dir,
+        'surveydata/demographics.tsv'),index_col=0,delimiter='\t')
+    healthdata=pandas.read_csv(os.path.join(derived_dir,
+        'surveydata/health_ordinal.tsv'),index_col=0,delimiter='\t')
+    alcdrugs=pandas.read_csv(os.path.join(derived_dir,
+        'surveydata/alcohol_drugs_ordinal.tsv'),index_col=0,delimiter='\t')
     assert all(demogdata.index==healthdata.index)
     assert all(demogdata.index==alcdrugs.index)
     demogdata=demogdata.merge(healthdata,left_index=True,right_index=True)
@@ -142,7 +143,10 @@ print('Demographic variables to test:')
 print(list(demogdata.columns))
 
 
-# First get binary variables and test classification based on survey data.  Only include variables that have at least 10% of the infrequent category. Some of these were not collected as binary variables, but we binarize by calling anything above the minimum value a positive outcome.
+# First get binary variables and test classification based on survey data.
+# Only include variables that have at least 10% of the infrequent category.
+# Some of these were not collected as binary variables, but we binarize by
+# calling anything above the minimum value a positive outcome.
 
 
 bvardata=numpy.array(demogdata)
@@ -151,16 +155,15 @@ sdata_orig=numpy.array(behavdata).copy()
 # just impute randomly here, since we don't have a way to work our
 # imputation into the CV loop
 
-nanvals=numpy.where(numpy.isnan(sdata_orig))
+#nanvals=numpy.where(numpy.isnan(sdata_orig))
 sdata=sdata_orig
-sdata[nanvals]=numpy.random.randn(nanvals[0].shape[0])*0.1
+#sdata[nanvals]=numpy.random.randn(nanvals[0].shape[0])*0.1
 results=pandas.DataFrame(columns=['variable','fa_ctr','trainf1','testf1'])
 
 clf_params={}
 
 ctr=0
 
-classifier='svm'
 accuracy={}
 imp={}
 null_accuracy={}
@@ -182,16 +185,21 @@ for varname in binary_vars:
 
     forest = ExtraTreesClassifier(n_estimators=250)
     outer_cv = StratifiedShuffleSplit(n_splits=5)
+    estimators = [('imputer',imputer),('reduce_dim', PCA()), ('clf',forest)]
+    pipeline=Pipeline(steps=estimators)
+    params=dict(reduce_dim=[None, FactorAnalysis(10), FactorAnalysis(25)])
 
+    grid_search = GridSearchCV(pipeline, param_grid=params)
     cvscores=[]
     nruns=10
-    nruns_shuf=10
+    nruns_shuf=250
     for i in range(nruns):
-        nested_score = cross_val_score(forest, X=sdata, y=y,
+        nested_score = cross_val_score(grid_search, X=sdata, y=y,
                             cv=outer_cv,scoring='roc_auc')
         cvscores.append(nested_score.mean())
     accuracy[varname]=cvscores
-    print(numpy.mean(accuracy[varname]),numpy.min(accuracy[varname]),numpy.max(accuracy[varname]))
+    print(numpy.mean(accuracy[varname]),
+            numpy.min(accuracy[varname]),numpy.max(accuracy[varname]))
 
     cvscores=[]
     # do shuffling to get null dist
@@ -199,7 +207,7 @@ for varname in binary_vars:
         y_shuf=y.copy()
         numpy.random.shuffle(y_shuf)
         assert not all(y_shuf==y)
-        nested_score = cross_val_score(forest, X=sdata, y=y_shuf,
+        nested_score = cross_val_score(pipeline, X=sdata, y=y_shuf,
                             cv=outer_cv,scoring='roc_auc')
         cvscores.append(nested_score.mean())
     null_accuracy[varname]=cvscores
@@ -208,7 +216,7 @@ for varname in binary_vars:
     assert all(y==numpy.array(demogdata[varname]))
 
     # fit on all data and compute importance
-    forest.fit(sdata, y)
+    forest.fit(fancyimpute.SoftImpute(verbose=False).complete(sdata), y)
     importances[varname] = forest.feature_importances_
     std = numpy.std([tree.feature_importances_ for tree in forest.estimators_],
                  axis=0)
@@ -217,13 +225,16 @@ for varname in binary_vars:
     # Print the feature ranking
     print("Feature ranking:")
     for f in range(5):
-        print("%s. feature %d (%f)" % (behavvars[indices[f]], indices[f], importances[varname][indices[f]]))
+        print("%s. feature %d (%f)" % (behavvars[indices[f]], indices[f],
+                            importances[varname][indices[f]]))
     print('')
 
 import pickle
-pickle.dump((accuracy,importances,null_accuracy,cutoff),open('behavpred_performance.pkl','wb'))
+pickle.dump((accuracy,importances,null_accuracy,cutoff),
+        open('behavpred_pipeline_performance.pkl','wb'))
 
-(accuracy,importances,null_accuracy,cutoff)=pickle.load(open('behavpred_performance.pkl','rb'))
+(accuracy,importances,null_accuracy,cutoff)=pickle.load(
+        open('behavpred_pipeline_performance.pkl','rb'))
 for varname in binary_vars:
     if not varname in importances:
         continue
@@ -231,11 +242,13 @@ for varname in binary_vars:
     print('')
     print('Variable:',varname)
     print('Prediction accuracy (ROC AUC):',numpy.mean(accuracy[varname][:10]))
-    pval=1-scipy.stats.percentileofscore(null_accuracy[varname],numpy.mean(accuracy[varname][:10]))/100.
+    pval=1-scipy.stats.percentileofscore(null_accuracy[varname],
+                numpy.mean(accuracy[varname][:10]))/100.
     print('null mean %f, pval: %f'%(numpy.mean(null_accuracy[varname]),pval))
     if pval<0.1:
         # Print the feature ranking
         print("Feature ranking:")
         for f in range(5):
-            print("%s. feature %d (%f)" % (behavvars[indices[f]], indices[f], importances[varname][indices[f]]))
+            print("%s. feature %d (%f)" % (behavvars[indices[f]], indices[f],
+                        importances[varname][indices[f]]))
         print('')
