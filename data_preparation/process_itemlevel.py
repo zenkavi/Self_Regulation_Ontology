bb16ca83c8d91eff76dae33a4b692ffaca8cae2d
poldrack <poldrack@gmail.com>
2016-10-16 13:41:20 -0700

    added extra surveys from 10-14 release

--- data_preparation/process_itemlevel.py
+++ data_preparation/process_itemlevel.py
@@ -20,6 +20,8 @@ if not os.path.exists(outdir):
     os.mkdir(outdir)
 
 survey_longnames={'eating_survey':'Eating survey',
+                  'selection_optimization_compensation_survey':'Selection-Optimization-Compensation survey',
+                  'sensation_seeking_survey':'Sensation seeking survey',
                   'erq_survey':'Emotion regulation questionnaire',
                   'impulsive_venture_survey':"I7 impulsiveness and venturesomeness survey",
                   'upps_impulsivity_survey':'UPPS+P',
@@ -61,7 +63,9 @@ survey_termurls={'erq_survey':"http://www.cognitiveatlas.org/term/id/trm_56bbead
                 'ten_item_personality_survey':"http://www.cognitiveatlas.org/term/id/trm_56a919a478935",
                 'dospert_rt_survey':"http://www.cognitiveatlas.org/term/id/trm_5696abecf2569",
                 'dickman_survey':"http://www.cognitiveatlas.org/term/id/trm_55a6a95f66508",
-                'mpq_control_survey':"http://www.cognitiveatlas.org/term/id/trm_55a6aa62c54f8"}
+                'mpq_control_survey':"http://www.cognitiveatlas.org/term/id/trm_55a6aa62c54f8",
+                'sensation_seeking_survey':'',
+                'selection_optimization_compensation_survey':''}
 
 
 def get_data(datadir=datadir):
