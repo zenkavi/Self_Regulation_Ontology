8c46d558473a7703bfbc6e02d550913f2065ae73
ian <ianeisenberg90@gmail.com>
2016-10-30 14:21:48 -0700

    added summary of measure importance to graph analysis

--- discovery_graph_analyses/graph_analysis.py
+++ discovery_graph_analyses/graph_analysis.py
@@ -6,6 +6,7 @@ from graph_utils import plot_graph, print_community_members, threshold_proportio
 from plot_utils import dendroheatmap
 from r_to_py_utils import missForest
 from utils import get_behav_data
+from data_preparation_utils import drop_vars
 
 import bct
 from collections import OrderedDict as odict
@@ -17,27 +18,30 @@ import pandas as pd
 import seaborn as sns
 
 # get dependent variables
-DV_df = get_behav_data('Discovery_10-21-2016', use_EZ = True)
+DV_df = get_behav_data('Discovery_10-21-2016', file = 'meaningful_variables_clean_cutoff2.50IQR.csv')
     
 # ************************************
 # ************ Imputation *******************
 # ************************************
 DV_df_imputed, error = missForest(DV_df)
 
+print('Finished imputing data')
+
+task_data = drop_vars(DV_df, ['survey'], saved_vars = ['cognitive_reflection'])
+
 # ************************************
 # ************ Connectivity Matrix *******************
 # ************************************
-graph_data = DV_df_imputed
+graph_data = task_data
 
 spearman_connectivity = calc_connectivity_mat(graph_data, edge_metric = 'spearman')
 distance_connectivity = calc_connectivity_mat(graph_data, edge_metric = 'distance')
 
-
+print('Finished creating connectivity matrices')
 # ************************************
 # ********* Heatmaps *******************
 # ************************************
 # dendrogram heatmap
-plot_df.columns = [' '.join(x.split('_')) for x in  plot_df.columns]
 fig = dendroheatmap(spearman_connectivity, labels = True)
 
 
@@ -78,24 +82,43 @@ c_a = bct.modularity_louvain_und_sign
 # circle layout                                                  
 G_spearman, connectivity_mat, visual_style = Graph_Analysis(spearman_connectivity, community_alg = c_a, thresh_func = t_f,
                                                      reorder = True, threshold = t,  layout = 'circle', 
-                                                     plot_threshold = plot_t, print_options = {'lookup': {}}, 
+                                                     plot_threshold = t, print_options = {'lookup': {}}, 
                                                     plot_options = {'inline': False})
 
 
+def node_importance(v):
+    return (v['subgraph_eigen_centrality'], v['community'])
+
+def integrate_over_measures(lst):
+    return np.sum(lst)
+
+def integrate_over_communities(lst):
+    return np.sum(lst)
     
-def get_top_community_tasks(G, community = 1):
-    vs = G.vs.select(community = community)
-    tasks = np.unique(list(map(lambda x: x.split('.')[0], vs['name'])))
-    task_importance = {}
+def get_task_importance(G):
+    tasks = np.unique(list(map(lambda x: x.split('.')[0], G.vs['name'])))
+    communities = range(1, max(G.vs['community'])+1)
+    task_importance_df = pd.DataFrame(index = tasks, columns = ['comm' + str(c) for c in communities] + ['num_measures'])
     for task in tasks:
-        measures = {v['name'].split('.')[1]:v['subgraph_eigen_centrality'] for v in vs if v['name'].split('.')[0] == task}
-        total_centrality = np.mean(list(measures.values()))
-        task_importance[task] = {'importance': total_centrality, 'measures': measures}
-    ordered_importance = odict(sorted(task_importance.items(), key = lambda t: t[1]['importance'], reverse = True))
-    return ordered_importance
+        measure_importance = [node_importance(v) for v in G.vs if v['name'].split('.')[0] == task]
+        community_importance = [integrate_over_measures([m[0] for m in measure_importance if m[1] == c]) for c in communities]
+        task_importance_df.loc[task,:] = community_importance + [len(measure_importance)]
+    return task_importance_df
     
-    
-                                                  
+df = get_task_importance(G_spearman)
+n_comms = df.shape[1]-1
+task_df = df.filter(regex = '^(?!.*survey).*$', axis = 0)
+sorted_list = (task_df.iloc[:,0:n_comms]).apply(lambda x: integrate_over_communities(x), axis = 1).sort_values(ascending = False)
+
+#plot task_df
+plot_df = task_df.iloc[:,0:n_comms]
+plot_df.loc[:,'task'] = plot_df.index
+plot_df = pd.melt(plot_df, id_vars = 'task', var_name = 'community', value_name = 'sum centrality')
+sns.set_context('poster')
+sns.barplot(x = 'community', y = 'sum centrality', data = plot_df, hue = 'task')
+leg = sns.plt.legend(bbox_to_anchor=(1.1, 1), loc='upper left', ncol=1)
+sns.plt.savefig('Plots/task_importance.pdf', bbox_inches = 'tight', pad_inches = 2)
+
 # save graph
 # distance graph
 t = 1
@@ -117,52 +140,7 @@ for gamma in np.arange(.5,2,.05):
         layout = visual_style['layout']
     ref_community = G_w.vs['community']
                                             
-                                            
-# spearman thresholded graph
-t = .5
-plot_t = .15
-t_f = bct.threshold_proportional
-
-
-layout = None
-ref_community = None
-for gamma in np.arange(.5,2,.05):
-    layout = layout or 'kk'
-    c_a = lambda x: bct.community_louvain(x, gamma = gamma)
-    G_w, connectivity_adj, visual_style = Graph_Analysis(spearman_connectivity, community_alg = c_a, ref_community = ref_community,
-                                                         thresh_func = t_f, threshold = t, plot_threshold = plot_t, 
-                                                         layout = layout, 
-                                                         print_options = {'lookup': {}, 'file': 'Plots/gamma=' + str(gamma) + '_weighted_spearman.txt'}, 
-                                                        plot_options = {'inline': False, 'target': 'Plots/gamma=' + str(gamma) + '_weighted_spearman.pdf'})
-    if type(layout) != igraph.layout.Layout:
-        layout = visual_style['layout']
-    ref_community = G_w.vs['community']
-
-                                    
-# spearman thresholded graph
-t = 1
-t_f = bct.threshold_proportional
-
-
-layout = None
-ref_community = None
-reorder = True
-graph_mat = spearman_connectivity
-for gamma in np.arange(.5,2,.05):
-    c_a = lambda x: bct.modularity_louvain_und_sign(x, gamma = gamma)
-    layout = layout or 'circle'
-    G_w, connectivity_adj, visual_style = Graph_Analysis(graph_mat, community_alg = c_a, ref_community = ref_community,
-                                                         thresh_func = t_f, threshold = t, plot_threshold = plot_t, 
-                                                         layout = layout, reorder = reorder,
-                                                         print_options = {'lookup': {}, 'file': 'Plots/gamma=' + str(gamma) + '_weighted_spearman_sign.txt'}, 
-                                                        plot_options = {'inline': False, 'target': 'Plots/gamma=' + str(gamma) + '_weighted_spearman_sign.pdf'})
-    if type(layout) != igraph.layout.Layout:
-        layout = visual_style['layout']
-        reorder = False
-        graph_mat = connectivity_adj
-    ref_community = G_w.vs['community']
-                                                    
-                                                    
+        
                                                     
                                                      
 subgraph = community_reorder(get_subgraph(G_w,2))
