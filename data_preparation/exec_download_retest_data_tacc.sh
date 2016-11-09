5c0c4c14503d08a96078385e79cf2665255c4d53
Zeynep Enkavi <zeynepenkavi@gmail.com>
2016-11-08 20:55:11 -0600

    fixed job script

--- data_preparation/exec_download_retest_data_tacc.sh
+++ data_preparation/exec_download_retest_data_tacc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /Users/zeynepenkavi/Dropbox/PoldrackLab/Self_Regulation_Ontology/data_preparation/download_retest_data_tacc.py "all" "['retest', 'incomplete']"
+python /corral-repl/utexas/poldracklab/users/zenkavi/Self_Regulation_Ontology/data_preparation/download_retest_data_tacc.py "all" "['retest', 'incomplete']"
