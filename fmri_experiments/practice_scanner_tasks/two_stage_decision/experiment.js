7687d5a5f2c8e7dff796425bc7801d1efbb2660f
poldracklab <poldracklab@Russs-Macbook-Air-2.local>
2016-11-10 15:41:25 -0800

    2 stage practice

--- fmri_experiments/practice_scanner_tasks/two_stage_decision/experiment.js
+++ fmri_experiments/practice_scanner_tasks/two_stage_decision/experiment.js
@@ -1,75 +1,9 @@
 /* ************************************ */
 /* Define helper functions */
 /* ************************************ */
-
-
 var getInstructFeedback = function() {
 	return '<div class = centerbox><p class = center-block-text>' + feedback_instruct_text +
 		'</p></div>'
-}
-
-function assessPerformance() {
-	/* Function to calculate the "credit_var", which is a boolean used to
-	credit individual experiments in expfactory. */
-	var experiment_data = jsPsych.data.getTrialsOfType('poldrack-single-stim')
-	var missed_count = 0
-	var trial_count = 0
-	var rt_array = []
-	var rt = 0
-		//record choices participants made
-	var choice_counts = {}
-	choice_counts[-1] = 0
-	for (var k = 0; k < choices.length; k++) {
-		choice_counts[choices[k]] = 0
-	}
-	for (var i = 0; i < experiment_data.length; i++) {
-		if (experiment_data[i].possible_responses != 'none') {
-			trial_count += 1
-			rt = experiment_data[i].rt
-			key = experiment_data[i].key_press
-			choice_counts[key] += 1
-			if (rt == -1) {
-				missed_count += 1
-			} else {
-				rt_array.push(rt)
-			}
-		}
-	}
-	//calculate average rt
-	var avg_rt = -1
-	if (rt_array.length !== 0) {
-		avg_rt = math.median(rt_array)
-	} 
-		//calculate whether response distribution is okay
-	var responses_ok = true
-	Object.keys(choice_counts).forEach(function(key, index) {
-		if (choice_counts[key] > trial_count * 0.85) {
-			responses_ok = false
-		}
-	})
-	var missed_percent = missed_count/trial_count
-	credit_var = (missed_percent < 0.4 && avg_rt > 200 && responses_ok)
-	if (credit_var === true) {
-	  performance_var = total_score
-	} else {
-	  performance_var = 0
-	}
-	jsPsych.data.addDataToLastTrial({"credit_var": credit_var, "performance_var": performance_var})
-}
-
-function evalAttentionChecks() {
-	var check_percent = 1
-	if (run_attention_checks) {
-		var attention_check_trials = jsPsych.data.getTrialsOfType('attention-check')
-		var checks_passed = 0
-		for (var i = 0; i < attention_check_trials.length; i++) {
-			if (attention_check_trials[i].correct === true) {
-				checks_passed += 1
-			}
-		}
-		check_percent = checks_passed / attention_check_trials.length
-	}
-	return check_percent
 }
 
 //Polar method for generating random samples from a norma distribution.
@@ -106,26 +40,6 @@ var get_current_trial = function() {
 var initialize_FB_matrix = function() {
 	return [Math.random() * 0.5 + 0.25, Math.random() * 0.5 + 0.25, Math.random() * 0.5 + 0.25, Math.random() * 0.5 + 0.25]
 }
-
-//Change phase from practice to test
-var change_phase = function() {
-	if (curr_images == practice_images) {
-		curr_images = test_images
-		curr_colors = test_colors
-		curr_fs_stims = test_fs_stims
-		curr_ss_stims = test_ss_stim
-		exp_stage = 'test'
-	} else {
-		curr_images = practice_images
-		curr_colors = practice_colors
-		curr_fs_stims = practice_fs_stims
-		curr_ss_stim = practice_ss_stim
-		exp_stage = 'practice'
-	}
-	total_score = 0
-	current_trial = -1 //reset count
-}
-
 
 /*
 Generate first stage stims. Takes in an array of images and colors (which change between practice anad test)
@@ -410,50 +324,9 @@ var curr_ss_stims = practice_ss_stim
 /* ************************************ */
 /* Set up jsPsych blocks */
 /* ************************************ */
-// Set up attention check node
-var attention_check_block = {
-	type: 'attention-check',
-	data: {
-		trial_id: 'attention_check'
-	},
-	timing_response: 180000,
-	response_ends_trial: true,
-	timing_post_trial: 200
-}
-
-var attention_node = {
-	timeline: [attention_check_block],
-	conditional_function: function() {
-		return run_attention_checks
-	}
-}
-
-//Set up post task questionnaire
-var post_task_block = {
-   type: 'survey-text',
-   data: {
-       trial_id: "post task questions"
-   },
-   questions: ['<p class = center-block-text style = "font-size: 20px">Please summarize what you were asked to do in this task.</p>',
-              '<p class = center-block-text style = "font-size: 20px">Do you have any comments about this task?</p>'],
-   rows: [15, 15],
-   columns: [60,60]
-};
-
-/* define static blocks */
-var attention_check_block = {
-	type: 'attention-check',
-	data: {
-		trial_id: 'attention_check'
-	},
-	timing_response: 180000,
-	response_ends_trial: true,
-	timing_post_trial: 200
-}
-
 
 var feedback_instruct_text =
-	'Welcome to the experiment. This task will take about 25 minutes. Press <strong>enter</strong> to begin.'
+	'Welcome to the practice phase for this experiment. Press <strong>enter</strong> to begin.'
 var feedback_instruct_block = {
 	type: 'poldrack-text',
 	data: {
@@ -513,7 +386,7 @@ var second_instructions_block = {
 	pages: [
 		'<div class = centerbox><p class = block-text>As you saw, you get feedback after your second-stage choice: either a gold coin or a red "0". The gold coins determine your bonus pay, so try to get as many as possible!</p><p class = block-text>As mentioned, there are four second-stage shapes: two shapes in 2a and two shapes in 2b. These four shapes each have a different chance of paying a gold coin. You want to learn which shape is the best so you can get as many coins as possible.</p></div>',
 		'<div class = centerbox><p class = block-text>The chance of getting a coin from each second-stage shape changes over the experiment, so the best choice early on may not be the best choice later.</p><p class = block-text>In contrast, the chance of going to one of the second-stages after choosing one of the first-stage choices is fixed throughout the experiment. If you find over time that one first-stage shape brings you to 2a most of the time, it will stay that way for the whole experiment.</p></div>',
-		'<div class = centerbox><p class = block-text>After you end instructions we will start with some practice.</p><p class = block-text>After practice we will show you the instructions again, but please make sure you understand them as well as you can now.</p></div>'
+		'<div class = centerbox><p class = block-text>After you end instructions we will start practice.</p><p class = block-text>After practice we will show you the instructions again, but please make sure you understand them as well as you can now.</p></div>'
 	],
 	allow_keys: false,
 	show_clickable_nav: true,
@@ -526,23 +399,12 @@ var end_block = {
 		trial_id: 'end',
     	exp_id: 'two_stage_decision'
 	},
-	text: '<div class = centerbox><p class = center-block-text>Thanks for completing this task!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
+	text: '<div class = centerbox><p class = center-block-text>Thanks for completing this practice!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
 	cont_key: [13],
 	timing_response: 180000,
 	timing_post_trial: 0,
-	on_finish: assessPerformance
 };
 
-var wait_block = {
-	type: 'poldrack-text',
-	data: {
-		trial_id: 'wait'
-	},
-	text: '<div class = centerbox><p class = center-block-text>Take a break!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
-	cont_key: [13],
-	timing_response: 120000,
-	timing_post_trial: 1000
-};
 
 var start_practice_block = {
 	type: 'poldrack-text',
@@ -555,16 +417,6 @@ var start_practice_block = {
 	timing_post_trial: 1000
 };
 
-var start_test_block = {
-	type: 'poldrack-text',
-	data: {
-		trial_id: 'test_intro'
-	},
-	text: "<div class = centerbox><p class = block-text>Finished with practice. We will now start the test with new shapes and stages.</p><p class = block-text>Just like in the practice, each first-stage choice is primarily associated with one second-stage and each second-stage shape has a different chance of earning a point. Each second-stage shape's chance of earning a gold coin changes over the expeirment, so the best shape early on may not be the best shape later. In contrast, once you learn which stage a first-stage choice brings you to most of the time, it will stay the same for the whole experiment.</p><p class = block-text>Your task is to earn as many gold coins as possible. Press <strong>enter</strong> to begin.</p></div>",
-	cont_key: [13],
-	timing_response: 180000,
-	timing_post_trial: 1000
-};
 
 var intertrial_wait_update_FB = {
 	type: "poldrack-single-stim",
@@ -590,14 +442,6 @@ var intertrial_wait = {
 	timing_response: 1000
 }
 
-var change_phase_block = {
-	type: 'call-function',
-	data: {
-		trial_id: 'change_phase'
-	},
-	func: change_phase,
-	timing_post_trial: 0
-}
 
 //experiment blocks
 var first_stage = {
@@ -723,7 +567,6 @@ two_stage_decision_experiment.push(noFB_node)
 //continue instructions
 two_stage_decision_experiment.push(second_instructions_block);
 two_stage_decision_experiment.push(start_practice_block);
-two_stage_decision_experiment.push(attention_node)
 for (var i = 0; i < practice_trials_num; i++) {
 	two_stage_decision_experiment.push(first_stage)
 	two_stage_decision_experiment.push(first_stage_selected)
@@ -731,26 +574,4 @@ for (var i = 0; i < practice_trials_num; i++) {
 	two_stage_decision_experiment.push(FB_node)
 	two_stage_decision_experiment.push(noFB_node)
 }
-two_stage_decision_experiment.push(attention_node)
-two_stage_decision_experiment.push(change_phase_block)
-two_stage_decision_experiment.push(start_test_block)
-for (var i = 0; i < test_trials_num / 2; i++) {
-	two_stage_decision_experiment.push(first_stage)
-	two_stage_decision_experiment.push(first_stage_selected)
-	two_stage_decision_experiment.push(second_stage)
-	two_stage_decision_experiment.push(FB_node)
-	two_stage_decision_experiment.push(noFB_node)
-}
-two_stage_decision_experiment.push(attention_node)
-two_stage_decision_experiment.push(wait_block)
-for (var i = 0; i < test_trials_num / 2; i++) {
-	two_stage_decision_experiment.push(attention_node)
-	two_stage_decision_experiment.push(first_stage)
-	two_stage_decision_experiment.push(first_stage_selected)
-	two_stage_decision_experiment.push(second_stage)
-	two_stage_decision_experiment.push(FB_node)
-	two_stage_decision_experiment.push(noFB_node)
-}
-two_stage_decision_experiment.push(attention_node)
-two_stage_decision_experiment.push(post_task_block)
 two_stage_decision_experiment.push(end_block)
