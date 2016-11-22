a9e38523b9d8c122693d09ba4afea9ef27442238
Zeynep Enkavi <zeynepenkavi@gmail.com>
2016-11-22 07:14:39 -0800

    Changed tac script to be able to submit smaller jobs

--- data_preparation/download_retest_data_tacc.py
+++ data_preparation/download_retest_data_tacc.py
@@ -7,16 +7,19 @@ import json
 import numpy as np
 from os import path
 import pandas as pd
+import time
 
 sys.path.append('/corral-repl/utexas/poldracklab/users/zenkavi/Self_Regulation_Ontology/utils')
 from data_preparation_utils import anonymize_data, calc_trial_order, convert_date, download_data, get_bonuses, get_pay,  remove_failed_subjects
 from utils import get_info
 
 if len(sys.argv) < 2:
-    sys.exit("Usage: download_retest_data_tacc.py 'all' '['retest', 'incomplete']'")
+    sys.exit("Usage: download_retest_data_tacc.py 'all' '['retest', 'incomplete']' 'http://expfactory.org/api/results' 'http://expfactory.org/api/results/?page=3' ")
 
 job = sys.argv[1]
 sample = sys.argv[2]
+url = sys.argv[3]
+last_url = sys.argv[4]
         
 #load Data
 token = get_info('expfactory_token', infile='/corral-repl/utexas/poldracklab/users/zenkavi/Self_Regulation_Ontology/Self_Regulation_Retest_Settings_Tacc.txt')
@@ -44,23 +47,23 @@ if job == 'download' or job == "all":
     #load Data
     f = open(token)
     access_token = f.read().strip()  
-    data = download_data(data_dir, access_token, filters = filters,  battery = 'Self Regulation Retest Battery')
+    data = download_data(data_dir, access_token, filters = filters,  battery = 'Self Regulation Retest Battery', url = url, last_url = last_url)
     data.reset_index(drop = True, inplace = True)
     
 if job in ['extras', 'all']:
     #Process Data
     if job == "extras":
         #load Data
-        data = pd.read_json(path.join(data_dir, 'mturk_data.json'))
+        data = pd.read_json(path.join(data_dir, 'mturk_data_'+time.strftime("%m_%d_%Y-%I_%M_%S")+'.json'))
         data.reset_index(drop = True, inplace = True)
         print('Finished loading raw data')
     
     #anonymize data
     worker_lookup = anonymize_data(data)
-    json.dump(worker_lookup, open(path.join(data_dir, 'worker_lookup.json','w')))
+    json.dump(worker_lookup, open(path.join(data_dir, 'worker_lookup_'+time.strftime("%m_%d_%Y-%I_%M_%S")+'.json','w')))
     
     # record subject completion statistics
-    (data.groupby('worker_id').count().finishtime).to_json(path.join(data_dir, 'worker_counts.json'))
+    (data.groupby('worker_id').count().finishtime).to_json(path.join(data_dir, 'worker_counts'+time.strftime("%m_%d_%Y-%I_%M_%S")+'.json'))
     
     # add a few extras
     convert_date(data)
@@ -70,81 +73,12 @@ if job in ['extras', 'all']:
     calc_trial_order(data)
     
     # save data
-    data.to_json(path.join(data_dir, 'mturk_data_extras.json'))
+    data.to_json(path.join(data_dir, 'mturk_data_extras'+time.strftime("%m_%d_%Y-%I_%M_%S")+'.json'))
     
     # calculate pay
     pay = get_pay(data)
-    pay.to_json(path.join(data_dir, 'worker_pay.json'))
+    pay.to_json(path.join(data_dir, 'worker_pay'+time.strftime("%m_%d_%Y-%I_%M_%S")+'.json'))
     print('Finished saving worker pay')
-    
-#if job in ['post', 'all']:
-#    #Process Data
-#    if job == "post":
-#        #load Data
-#        try:
-#            data = pd.read_json(path.join(data_dir, 'mturk_data_extras.json'))
-#        except ValueError:
-#            data = pd.read_json(path.join(data_dir,  'mturk_data.json'))
-#        data.reset_index(drop = True, inplace = True)
-#        print('Finished loading raw data')
-#    
-#    #get subject assignment
-#    # subject_assignment = pd.read_csv('samples/subject_assignment.csv', index_col = 0)
-#    subject_assignment_retest = pd.read_csv('samples/subject_assignment_retest.csv', index_col = 0)
-#    # discovery_sample = list(subject_assignment.query('dataset == "discovery"').index)
-#    # validation_sample = list(subject_assignment.query('dataset == "validation"').index)
-#    retest_sample = list(subject_assignment_retest.query('dataset == "retest"').index)
-#    # extra_sample =  ['s' + str(i) for i in range(501,600)]
-#    extra_sample =  ['s' + str(i) for i in range(51,100)]
-#    
-#    # create dataframe to hold failed data
-#    failed_data = pd.DataFrame()
-#    # preprocess extras
-#    # only get extra data
-#    extra_data = data.query('worker_id in %s' % extra_sample).reset_index(drop = True)
-#    post_process_data(extra_data)
-#    failures = remove_failed_subjects(extra_data)
-#    failed_data = pd.concat([failed_data,failures])
-#    extra_workers = np.sort(extra_data.worker_id.unique())
-#    print('Finished processing extra data')    
-#    
-#    if 'retest' in sample:
-#        # only get retest data
-#        retest_data = data.query('worker_id in %s' % retest_sample).reset_index(drop = True)
-#        post_process_data(retest_data)
-#        failures = remove_failed_subjects(retest_data)
-#        failed_data = pd.concat([failed_data,failures])
-#        # add extra workers if necessary
-#        num_failures = len(failures.worker_id.unique())
-#        if num_failures > 0:
-#            makeup_workers = extra_workers[0:num_failures]
-#            new_data = extra_data[extra_data['worker_id'].isin(makeup_workers)]
-#            retest_data = pd.concat([retest_data, new_data]).reset_index(drop = True)
-#            extra_data.drop(new_data.index, inplace = True)
-#        retest_data.to_json(path.join(data_dir,'mturk_retest_data_post.json'))
-#        print('Finished saving post-processed retest data')
-#
-#    if 'incomplete' in sample:
-#        # only get incomplete data
-#        # incomplete_data = data.query('worker_id not in %s' % (validation_sample + discovery_sample + extra_sample)).reset_index(drop = True)
-#        incomplete_data = data.query('worker_id not in %s' % (retest_sample + extra_sample)).reset_index(drop = True)
-#        post_process_data(incomplete_data)
-#        remove_failed_subjects(incomplete_data)
-#        incomplete_data.to_json(data_dir + 'mturk_incomplete_data_post.json')
-#        print('Finished saving post-processed incomplete data')
-#    # save failed data
-#    failed_data = failed_data.reset_index(drop = True)
-#    failed_data.to_json(data_dir + 'mturk_failed_data_post.json')
-#    print('Finished saving post-processed failed data')
-#    
-#if job in ['DV', 'all']:
-#    # for sample in ['discovery', 'failed']:
-#    for sample in ['retest', 'failed']:
-#        data = pd.read_json(data_dir + 'mturk_' + sample + '_data_post.json')
-#        #calculate DVs
-#        DV_df, valence_df = extract_DVs(data)
-#        DV_df.to_json(path.join(data_dir, 'mturk_' + sample + '_DV.json'))
-#        valence_df.to_json(path.join(data_dir, 'mturk_' + sample + '_DV_valence.json'))
 
     
     
