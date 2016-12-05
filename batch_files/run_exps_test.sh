dcdd7bc289b1b168b5826e7dbed2ce06d8a03b10
Ian Weiss Eisenberg <ieisenbe@sherlock-ln02.stanford.edu>
2016-12-04 21:46:21 -0800

    updated batch files for multiple samples

--- batch_files/run_exps_test.sh
+++ batch_files/run_exps_test.sh
@@ -1,6 +1,6 @@
 set -e
 for exp_id in threebytwo
 do
-sed "s/{EXP_ID}/$exp_id/g" calculate_DVs.batch | sbatch -p russpold
+sed "s/{EXP_ID}/$exp_id/g" calculate_discovery_DVs.batch | sbatch -p russpold
 done
 
