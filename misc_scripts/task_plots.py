9ea6a18703da916bc6a2e5cd10cbca0aa7e272fe
ian <ianeisenberg90@gmail.com>
2016-12-08 13:33:05 -0800

    updated to make plot directory

--- misc_scripts/task_plots.py
+++ misc_scripts/task_plots.py
@@ -1,12 +1,18 @@
+import os
 import numpy as np
 import sys
 sys.path.append('../utils')
 from utils import get_behav_data
 import seaborn as sns
 
+#Make Plot Directy if it doesn't exist
+if not os.path.exists('Plots'):
+    os.mkdir('Plots')
+
 # get DV df
 DV_df = get_behav_data()
 tasks = np.unique(DV_df.columns.map(lambda x: x.split('.')[0]))
+
 for task in tasks:
     subset = DV_df.filter(regex = '^%s' % task)
     subset = subset.dropna(how = 'all').dropna(axis = 1)
