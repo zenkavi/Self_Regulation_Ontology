70fbd668e254bc531790745d8669958712ef4640
Russell Poldrack <poldrack@gmail.com>
2016-12-10 14:42:22 -0600

    normalize lasso

--- task_selection/search_objectives.py
+++ task_selection/search_objectives.py
@@ -74,7 +74,7 @@ def get_reconstruction_error(ct,taskdata,targetdata_orig,params):
     elif params.clf=='lassocv':
         linreg=MultiTaskLassoCV(alphas=[10**x for x in numpy.arange(-6,8)])
     elif params.clf=='lasso':
-        linreg=Lasso(alpha=params.lasso_alpha)
+        linreg=Lasso(alpha=params.lasso_alpha,normalize=True)
     elif params.clf=='rf':
         linreg=RandomForestRegressor()
     else:
