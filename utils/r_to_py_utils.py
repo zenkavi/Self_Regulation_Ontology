b08fb691af937739a90a6bdc733ddf5f1ba6cc6c
ian <ianeisenberg90@gmail.com>
2016-12-21 23:51:27 -0800

    added rpy2 psych function

--- utils/r_to_py_utils.py
+++ utils/r_to_py_utils.py
@@ -18,5 +18,22 @@ def GPArotation(data, method='varimax', normalize=True):
     rotated_data = GPArotation.GPForth(data.values, method = method, normalize=normalize)[0]
     rotated_data = pd.DataFrame(data = np.matrix(rotated_data), index=data.index, columns=data.columns)
     return rotated_data
-    
+
+def psychFA(data, n_components, return_attrs = ['BIC', 'RMSEA']):
+    def get_attr(attr):
+        try:
+            index = list(fa.names).index(attr)
+            val = list(fa.items())[index][1]
+            if len(val) == 1:
+                val = val[0]
+            return val
+        except ValueError:
+            print('Did not pass a valid attribute')
+    psych = importr('psych')
+    fa = psych.fac(data, n_components)
+    attr_dic = {}
+    attr_dic['loadings'] = np.matrix(get_attr('loadings'))
+    for attr in return_attrs:
+        attr_dic[attr] = get_attr(attr)
+    return attr_dic
     
