0bc2bf7cfb4423eeb339be9cd6c4d6f17f5b974b
poldrack <poldrack@gmail.com>
2016-12-30 16:52:53 -0800

    include R-based models

--- prediction_analyses/behavpredict.py
+++ prediction_analyses/behavpredict.py
@@ -21,14 +21,16 @@ Russ Poldrack
 import sys,os
 import random
 import pickle
+import warnings
 
-import numpy
+import numpy,pandas
+import importlib
 
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import cross_val_score,StratifiedKFold,ShuffleSplit,GridSearchCV
 from sklearn.metrics import roc_auc_score,r2_score
 from sklearn.linear_model import LassoCV
-from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
 
 import fancyimpute
 from imblearn.combine import SMOTETomek
@@ -36,16 +38,19 @@ from imblearn.combine import SMOTETomek
 sys.path.append('../utils')
 from utils import get_info,get_behav_data,get_demographics
 
-
+from get_balanced_folds import BalancedKFold
+import prediction_utils
+importlib.reload(prediction_utils)
 
 class BehavPredict:
     def __init__(self,verbose=False,dataset=None,
                     use_full_dataset=True,
                     drop_na_thresh=50,
-                    binary_min_proportion=0.05,
-                    categorical_vars=None,n_jobs=1,
+                    n_jobs=1,
+                    categorical_vars=None,
                     n_outer_splits=4,
-                    use_smote=True,smote_cutoff=0.1):
+                    use_smote=True,smote_cutoff=0.1,
+                    baseline_vars=['Age','Sex']):
         # set up arguments
         self.verbose=verbose
         if not dataset:
@@ -66,18 +71,21 @@ class BehavPredict:
                                 'OtherDrugs', 'OtherRace', 'OtherTobaccoProducts',
                                 'PsychDiagnoses',
                                 'PsychDiagnosesOther']
-        self.binary_min_proportion=binary_min_proportion
         self.n_jobs=n_jobs
         self.n_outer_splits=n_outer_splits
+        self.baseline_vars=baseline_vars
+
         # define internal variables
         self.demogdata=None
         self.behavdata=None
         self.dropped_na_columns=None
         self.binary_cutoffs={}
-        self.rocscores={}
+        self.scores={}
         self.importances={}
         self.use_smote=use_smote
         self.smote_cutoff=smote_cutoff
+        self.data_models={}
+        self.pred=None
 
     def load_demog_data(self,cleanup=True,binarize=False,
                         drop_categorical=True):
@@ -108,6 +116,14 @@ class BehavPredict:
                 if self.verbose:
                     print('dropping categorical variable:',v)
 
+
+    def get_demogdata_vartypes(self,zinf_cutoff=0.5):
+        # for each variable, get info on how it is distributed
+        with open('demographic_model_type.txt') as f:
+            lines=[i.strip().split() for i in f.readlines()]
+            for l in lines:
+                self.data_models[l[0]]=l[1]
+
     def load_behav_data(self,datasubset='all'):
         self.behavdata=get_behav_data(self.dataset,
                                 'meaningful_variables_clean.csv',
@@ -124,6 +140,11 @@ class BehavPredict:
                     del self.behavdata[v]
                     if self.verbose>1:
                         print('dropping non-survey var:',v)
+        if datasubset=='baseline':
+            self.behavdata=self.demogdata[self.baseline_vars].copy()
+        else:
+            for v in self.baseline_vars:
+                self.behavdata[v]=self.demogdata[v].copy()
 
         if self.drop_na_thresh>0:
             na_count=numpy.sum(numpy.isnan(self.behavdata),0)
@@ -137,6 +158,7 @@ class BehavPredict:
                 print('dropping %d vars due to excessive NAs'%len(self.dropped_na_columns))
                 print('%d behavioral variables remaining'%self.behavdata.shape[1])
 
+
     def get_joint_datasets(self):
         demog_index=set(self.demogdata.index)
         behav_index=set(self.behavdata.index)
@@ -147,6 +169,7 @@ class BehavPredict:
         if self.verbose:
             print('datasets successfully aligned')
             print('%d subjects in joint dataset'%self.demogdata.shape[0])
+
 
     def binarize_demog_vars(self):
         print('binarizing demographic data...')
@@ -167,17 +190,32 @@ class BehavPredict:
                 else:
                     self.demogdata[v]=(self.demogdata[v]>c).astype('int')
 
+
     def run_crossvalidation(self,v,clf=None,outer_cv=None,
                             imputer=fancyimpute.SimpleFill,
                             shuffle=False,scoring='roc_auc'):
         """
         v is the variable on which to run crosvalidation
         """
+
+        if self.data_models[v]=='binary':
+            return self.run_crossvalidation_binary(v,LassoCV(),outer_cv,
+                                                imputer,shuffle,scoring)
+        else:
+            return self.run_crossvalidation_regression(v,clf,outer_cv,
+                                                imputer,shuffle,scoring='r2')
+
+
+    def run_crossvalidation_binary(self,v,clf=None,outer_cv=None,
+                            imputer=fancyimpute.SimpleFill,
+                            shuffle=False,scoring='roc_auc'):
+        """
+        run CV for binary data
+        """
+
         if self.verbose:
             print('classifying',v,numpy.mean(self.demogdata[v]))
-        if not clf:
-            clf = ExtraTreesClassifier(n_estimators=250,n_jobs=self.n_jobs,
-                                            class_weight='balanced')
+
         # set up crossvalidation
         if not outer_cv:
             outer_cv=StratifiedKFold(n_splits=self.n_outer_splits,shuffle=True)
@@ -191,9 +229,19 @@ class BehavPredict:
         Xdata=Xdata.values
         scores=[]
         importances=[]
+        self.pred=numpy.zeros(Ydata.shape[0])
+        scale=StandardScaler()
         for train,test in outer_cv.split(Xdata,Ydata):
-            Xtrain=imputer().complete(Xdata[train,:])
-            Xtest=imputer().complete(Xdata[test,:])
+            Xtrain=Xdata[train,:]
+            Xtest=Xdata[test,:]
+            if numpy.sum(numpy.isnan(Xtrain))>0:
+                Xtrain=imputer().complete(Xdata[train,:])
+            if numpy.sum(numpy.isnan(Xtest))>0:
+                Xtest=imputer().complete(Xdata[test,:])
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", message="Data with input dtype int64 was converted to float64 by StandardScaler.")
+                Xtrain=scale.fit_transform(Xtrain)
+                Xtest=scale.transform(Xtest)
             Ytrain=Ydata[train]
             if numpy.abs(numpy.mean(Ytrain)-0.5)>self.smote_cutoff and self.use_smote:
                 if self.verbose>1:
@@ -201,13 +249,13 @@ class BehavPredict:
                 smt = SMOTETomek()
                 Xtrain,Ytrain=smt.fit_sample(Xtrain.copy(),Ydata[train])
             clf.fit(Xtrain,Ytrain)
-            pred=clf.predict(Xtest)
+            self.pred[test]=clf.predict(Xtest)
 
-        if numpy.var(pred)>0:
+        if numpy.var(self.pred)>0:
             if scoring=='r2':
-                scores=r2_score(Ydata[test],pred)
+                scores=r2_score(Ydata,self.pred)
             elif scoring=='roc_auc':
-                scores=roc_auc_score(Ydata[test],pred)
+                scores=roc_auc_score(Ydata,self.pred)
         else:
            if self.verbose:
                print(v,'zero variance in predictions')
@@ -216,6 +264,67 @@ class BehavPredict:
             importances.append(clf.feature_importances_)
         elif hasattr(clf,'coef_'):  # for lasso
             importances.append(clf.coef_)
+        if self.verbose:
+            print('mean accuracy = %0.3f'%scores)
+        try:
+            imp=numpy.vstack(importances)
+        except:
+            imp=None
+        return scores,imp
+
+    def run_crossvalidation_regression(self,v,clf=None,outer_cv=None,
+                            imputer=fancyimpute.SimpleFill,
+                            shuffle=False,scoring='r2'):
+        """
+        run CV for binary data
+        """
+        if self.verbose:
+            print('%s regression on'%self.data_models[v],v,numpy.mean(self.demogdata[v]>0))
+            clf=LassoCV()
+        if not self.data_models[v]=='gaussian':
+            print('using R to fit model...')
+            clf=prediction_utils.RModel(self.data_models[v])
+        # set up crossvalidation
+        if not outer_cv:
+            outer_cv=BalancedKFold()
+        Ydata=self.demogdata[v].dropna().copy()
+        Xdata=self.behavdata.loc[Ydata.index,:].copy()
+        Ydata=Ydata.values
+        if shuffle:
+            if self.verbose:
+                print('shuffling Y variable')
+                numpy.random.shuffle(Ydata)
+        Xdata=Xdata.values
+        scores=[]
+        importances=[]
+        self.pred=numpy.zeros(Xdata.shape[0])
+        scale=StandardScaler()
+        for train,test in outer_cv.split(Xdata,Ydata):
+            Xtrain=Xdata[train,:]
+            Xtest=Xdata[test,:]
+            if numpy.sum(numpy.isnan(Xtrain))>0:
+                Xtrain=imputer().complete(Xdata[train,:])
+            if numpy.sum(numpy.isnan(Xtest))>0:
+                Xtest=imputer().complete(Xdata[test,:])
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", message="Data with input dtype int64 was converted to float64 by StandardScaler.")
+                Xtrain=scale.fit_transform(Xtrain)
+                Xtest=scale.transform(Xtest)
+            Ytrain=Ydata[train]
+            clf.fit(Xtrain,Ytrain)
+            self.pred[test]=clf.predict(Xtest)
+
+        if numpy.var(self.pred)>0:
+            scores=numpy.corrcoef(Ydata,self.pred)[0,1]
+        else:
+           if self.verbose:
+               print(v,'zero variance in predictions')
+           scores=numpy.nan
+        if hasattr(clf,'feature_importances_'):  # for random forest
+            importances.append(clf.feature_importances_)
+        elif hasattr(clf,'coef_'):  # for lasso
+            importances.append(clf.coef_)
+
         if self.verbose:
             print('mean accuracy = %0.3f'%scores)
         try:
