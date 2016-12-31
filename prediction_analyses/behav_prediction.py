27b657d315c674cad8bd9177ecfd9a27e918ba9e
poldrack <poldrack@gmail.com>
2016-12-30 16:40:58 -0800

    workign version with tailored model types

---
+++ prediction_analyses/behav_prediction.py
@@ -0,0 +1,94 @@
+"""
+perform prediction on demographic data
+
+use different strategy depending on the nature of the variable:
+- lasso classification (logistic regression) for binary variables
+- lasso regression for normally distributed variables
+- lasso-regularized zero-inflated poisson regression for zero-inflated variables
+-- via R mpath library using rpy2
+
+compare each model to a baseline with age and sex as regressors
+
+"""
+
+import sys,os
+import random
+import pickle
+import importlib
+
+import numpy
+
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.linear_model import LassoCV
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
+import behavpredict
+importlib.reload(behavpredict)
+
+
+if __name__=='__main__':
+
+    # parameters to set
+    report_features=False
+    if len(sys.argv)>1:
+        shuffle=int(sys.argv[1])
+    else:
+        shuffle=False
+    if len(sys.argv)>2:
+        datasubset=sys.argv[3]
+    else:
+        datasubset='task'
+
+    output_dir='prediction_outputs'
+
+
+    assert datasubset in ['survey','mirt','task','all','baseline']
+
+    # set up classifier
+    clf=LassoCV()
+
+    bp=behavpredict.BehavPredict(verbose=2,n_jobs=23)
+    bp.load_demog_data()
+    bp.get_demogdata_vartypes()
+    bp.load_behav_data(datasubset)
+    if shuffle:
+        tmp=bp.demogdata.values.copy()
+        numpy.random.shuffle(tmp)
+        bp.demogdata.iloc[:,:]=tmp
+        print('WARNING: shuffling target data')
+    bp.get_joint_datasets()
+
+    vars_to_test=bp.demogdata.columns
+    #vars_to_test=['BMI']
+    for v in vars_to_test:
+        bp.scores[v],bp.importances[v]=bp.run_crossvalidation(v)
+        if report_features and numpy.mean(bp.scores[v])>0.65:
+            print('')
+            meanimp=numpy.mean(bp.importances[v],0)
+            meanimp_sortidx=numpy.argsort(meanimp)
+            for i in meanimp_sortidx[-1:-4:-1]:
+                print(bp.behavdata.columns[i],meanimp[i])
+            for i in meanimp_sortidx[:3][::-1]:
+                print(bp.behavdata.columns[i],meanimp[i])
+
+    h='%08x'%random.getrandbits(32)
+    shuffle_flag='shuffle_' if shuffle else ''
+    outfile='prediction_%s_%s%s.pkl'%(datasubset,shuffle_flag,h)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    pickle.dump((bp.scores,bp.importances),open(os.path.join(output_dir,outfile),'wb'))
+
+    # print a report
+
+    for v in vars_to_test:
+        t=bp.data_models[v]
+        if t=='binary':
+            cutoff=0.65
+        else:
+            cutoff=0.15
+
+        if bp.scores[v]<cutoff:
+            continue
+
+        print('%s\t%s\t%f\t(%s)'%(v,t,bp.scores[v],'\t'.join(['%f'%i for i in bp.importances[v].tolist()[0]])))
