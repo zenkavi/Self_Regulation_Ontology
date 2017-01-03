4cfa4f260065cf77119e0a425931158487248930
poldrack <poldrack@gmail.com>
2017-01-03 15:50:36 -0800

    add argparse agument handling

--- prediction_analyses/behav_prediction.py
+++ prediction_analyses/behav_prediction.py
@@ -27,54 +27,72 @@ from selfregulation.utils.utils import get_info
 import selfregulation.prediction.behavpredict as behavpredict
 importlib.reload(behavpredict)
 
+import argparse
+
 if __name__=='__main__':
 
-    # parameters to set
-    report_features=False
-    if len(sys.argv)>1:
-        shuffle=int(sys.argv[1])
-    else:
-        shuffle=False
-    if len(sys.argv)>2:
-        datasubset=sys.argv[2]
-    else:
-        datasubset='baseline'
-    if len(sys.argv)>3:
-        vars=[sys.argv[3]]
-    else:
-        vars=None
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", help="increase output verbosity",
+                        type=int,default=0)
+    parser.add_argument("--report_features", help="print features",
+                        action='store_true')
+    parser.add_argument("--print_report", help="print report at the end",
+                        action='store_true')
+    parser.add_argument('-s',"--shuffle", help="shuffle target variable",
+                        action='store_true')
+    parser.add_argument('-d',"--dataset", help="dataset for prediction",
+                            required=True)
+    parser.add_argument('-j',"--n_jobs", help="number of processors",type=int,
+                            default=2)
+    parser.add_argument('-w',"--workdir", help="working directory")
+    parser.add_argument('-r',"--resultsdir", help="results directory")
+    parser.add_argument('--nargs', nargs='+')
+    parser.add_argument("--singlevar", nargs='*',help="run with single variables")
 
-    try:
-        output_base=get_info('results_directory')
-    except:
-        output_base='.'
+    args=parser.parse_args()
+    print(args)
+    print(args.dataset)
+
+    # parameters to set
+
+    if args.resultsdir is None:
+        try:
+            output_base=get_info('results_directory')
+        except:
+            output_base='.'
+    else:
+        output_base=args.resultsdir
     output_dir=os.path.join(output_base,'prediction_outputs')
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    assert datasubset in ['survey','mirt','task','all','baseline']
+    assert args.dataset in ['survey','mirt','task','all','baseline']
 
     # set up classifier
     clf=LassoCV()
 
     # skip RetirementPercentStocks because it crashes the estimation tool
-    bp=behavpredict.BehavPredict(verbose=2,
-         drop_na_thresh=100,n_jobs=2,
+    bp=behavpredict.BehavPredict(verbose=args.verbose,
+         drop_na_thresh=100,n_jobs=args.n_jobs,
          skip_vars=['RetirementPercentStocks'])
     bp.load_demog_data()
     bp.get_demogdata_vartypes()
-    bp.load_behav_data(datasubset)
-    if shuffle:
+    bp.load_behav_data(args.dataset)
+    if args.shuffle:
         tmp=bp.demogdata.values.copy()
         numpy.random.shuffle(tmp)
         bp.demogdata.iloc[:,:]=tmp
         print('WARNING: shuffling target data')
     bp.get_joint_datasets()
 
-    if not vars:
+    if not args.singlevar:
         vars_to_test=bp.demogdata.columns
+        vars_to_test=['RelationshipNumber']
     else:
-        vars_to_test=vars
+        vars_to_test=args.singlevar
+
+    print(vars_to_test)
+
     for v in vars_to_test:
         if v in bp.skip_vars:
             print('skipping',v)
@@ -84,7 +102,7 @@ if __name__=='__main__':
             continue
         try:
             bp.scores[v],bp.importances[v]=bp.run_crossvalidation(v)
-            if report_features and numpy.mean(bp.scores[v])>0.65:
+            if args.report_features and numpy.mean(bp.scores[v])>0.65:
                 print('')
                 meanimp=numpy.mean(bp.importances[v],0)
                 meanimp_sortidx=numpy.argsort(meanimp)
@@ -97,22 +115,25 @@ if __name__=='__main__':
             print('error on',v,':',e)
 
     h='%08x'%random.getrandbits(32)
-    shuffle_flag='shuffle_' if shuffle else ''
-    outfile='prediction_%s_%s%s.pkl'%(datasubset,shuffle_flag,h)
+    shuffle_flag='shuffle_' if args.shuffle else ''
+    varflag='_'.join(args.singlevar) if args.singlevar else ''
+    outfile='prediction_%s_%s%s%s.pkl'%(args.dataset,shuffle_flag,varflag,h)
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     pickle.dump((bp.scores,bp.importances),open(os.path.join(output_dir,outfile),'wb'))
 
     # print a report
+    if args.print_report:
+        for v in vars_to_test:
+            if not v in bp.scores:
+                continue
+            t=bp.data_models[v]
+            if t=='binary':
+                cutoff=0.65
+            else:
+                cutoff=0.15
 
-    for v in vars_to_test:
-        t=bp.data_models[v]
-        if t=='binary':
-            cutoff=0.65
-        else:
-            cutoff=0.15
+            if bp.scores[v]<cutoff:
+                continue
 
-        if bp.scores[v]<cutoff:
-            continue
-
-        print('%s\t%s\t%f\t(%s)'%(v,t,bp.scores[v],'\t'.join(['%f'%i for i in bp.importances[v].tolist()[0]])))
+            print('%s\t%s\t%f\t(%s)'%(v,t,bp.scores[v],'\t'.join(['%f'%i for i in bp.importances[v].tolist()[0]])))
