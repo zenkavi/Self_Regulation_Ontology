490196c8b00e82dc6114af1bc37cea54f6921752
ian <ianeisenberg90@gmail.com>
2017-01-17 14:54:03 -0800

    added directory for design optimization

---
+++ fmri_experiments/design_files/get_stim_order.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 11:50:26 2017
+
+@author: ian
+"""
+from glob import glob
+import numpy as np
+import os
+import seaborn
+import sys
+
+task = sys.argv[1]
+
+def get_blocks(order):
+    blocks = []
+    previous_stim = None
+    block_len = 0
+    for i in stim_order:
+        if i != previous_stim and previous_stim != None:
+            blocks.append(block_len)
+            block_len = 1
+        else:
+            block_len+=1
+        previous_stim = i
+    return blocks
+
+block_counts = []
+for directory in glob(os.path.join(task,'design*')):
+    stim_onsets = []
+    stims = []
+    stim_i = 0
+    for stim_file in glob(os.path.join(directory,'stimulus*')):
+        stim_onset=list(np.loadtxt(stim_file))
+        stim_onsets+=stim_onset
+        stims+= [stim_i]*len(stim_onset)
+        stim_i+=1
+    sort_index = np.argsort(stim_onsets)
+    stim_order = [stims[i] for i in sort_index]
+    np.savetxt(os.path.join(directory,'stim_order.txt'),stim_order)
+    blocks = get_blocks(stim_order)
+    block_counts.append(blocks)
+
+
+
+f = seaborn.plt.figure()
+for i,block in enumerate(block_counts):
+    seaborn.plt.subplot(2,2,i+1)
+    seaborn.plt.hist(block)
+f.suptitle(task + ' Block Histogram', fontsize = 16)
+f.savefig(os.path.join(task,'task_block_histogram.pdf'))
