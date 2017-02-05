25d34e32b92cb2499b1c14a0484042e6fe9757d5
Ian <ianeisenberg90@gmail.com>
2017-02-04 23:02:00 -0800

    changed instructions style

--- fmri_experiments/scanner_tasks_set1/stop_signal/experiment.js
+++ fmri_experiments/scanner_tasks_set1/stop_signal/experiment.js
@@ -152,14 +152,16 @@ var correct_responses = jsPsych.randomization.shuffle([possible_responses[0], po
 	possible_responses[1], possible_responses[1]
 ])
 
-var tab = '&nbsp&nbsp&nbsp&nbsp'
 
-var prompt_text = '<ul list-text><li><img class = prompt_stim src = ' + images[0] + '></img>' + tab +
-	correct_responses[0][0] + '</li><li><img class = prompt_stim src = ' + images[1] + '></img>' + tab +
-	correct_responses[1][0] + ' </li><li><img class = prompt_stim src = ' + images[2] + '></img>   ' +
-	tab + correct_responses[2][0] +
-	' </li><li><img class = prompt_stim src = ' + images[3] + '></img>' + tab + correct_responses[3][0] +
-	' </li></ul>'
+var prompt_text = '<ul list-text>' + 
+					'<li><div class = prompt_container><img class = prompt_stim src = ' + 
+					images[0] + '></img>' + correct_responses[0][0] + '</div></li>' +
+					'</li><li><div class = prompt_container><img class = prompt_stim src = ' +
+					images[1] + '></img>'  + correct_responses[1][0] + '</div></li>' +
+					' </li><li><div class = prompt_container><img class = prompt_stim src = ' 
+					+ images[2] + '></img>' + correct_responses[2][0] + '</div></li>' +
+					' </li><li><div class = prompt_container><img class = prompt_stim src = ' +
+					images[3] + '></img>' + correct_responses[3][0] + '</div></li></ul>'
 
 /* Global task variables */
 var current_trial = 0
