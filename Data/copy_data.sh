ba36f6d3444597eca71c26d733d57e7fc703b8cf
Ian <ianeisenberg90@gmail.com>
2017-05-14 01:11:30 -0700

    created new datasets

--- Data/copy_data.sh
+++ Data/copy_data.sh
@@ -4,6 +4,6 @@ sshfs sherlock:/scratch/users/ieisenbe/ /mnt/Sherlock_Scratch
 #yes | cp -rf /mnt/Sherlock_Scratch/Self_Regulation_Ontology/Data/worker_pay.json Local
 yes | cp -rf /mnt/Sherlock_Scratch/Self_Regulation_Ontology/Data/*DV.json Local
 yes | cp -rf /mnt/Sherlock_Scratch/Self_Regulation_Ontology/Data/*DV_valence.json Local
-#yes | cp -rf /mnt/Sherlock_Scratch/Self_Regulation_Ontology/Data/*post* Local
+yes | cp -rf /mnt/Sherlock_Scratch/Self_Regulation_Ontology/Data/*post* Local
 #yes | cp -rf /mnt/Sherlock_Scratch/Self_Regulation_Ontology/Data/*discovery*raw* Local
 sudo umount /mnt/Sherlock_Scratch
--- dimensional_structure/graph_analyses/glasso_graph_analysis.py
+++ dimensional_structure/graph_analyses/glasso_graph_analysis.py
@@ -1,9 +1,10 @@
-
+import bct
 from os.path import join
 import seaborn as sns
 from selfregulation.utils.r_to_py_utils import qgraph_cor
 from selfregulation.utils.utils import get_behav_data, get_info
 from selfregulation.utils.plot_utils import dendroheatmap_left
+from selfregulation.utils.graph_utils import  Graph_Analysis
 
 base_directory = get_info('base_directory')
 plot_dir = join(base_directory,'dimensional_structure','Plots')
@@ -19,3 +20,13 @@ sns.plt.scatter(cor.as_matrix().flatten(),gcor.as_matrix().flatten())
 # plot heatmaps
 fig,leaves = dendroheatmap_left(gcor, label_fontsize=6)
 fig.savefig(join(plot_dir,'gcor_dendroheatmap.pdf'))
+
+
+
+GA = Graph_Analysis()
+GA.setup(data = gcor, 
+         thresh_func = bct.threshold_absolute,
+         community_alg = bct.modularity_louvain_und_sign)
+GA.calculate_communities()
+GA.set_visual_style(layout='kk', plot_threshold=0)
+GA.display()
