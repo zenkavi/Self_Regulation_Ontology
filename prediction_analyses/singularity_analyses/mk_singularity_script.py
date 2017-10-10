63316f8b29c8a4aa71f8c178885e42e4054bd654
Russ Poldrack <poldrack@gmail.com>
2017-10-10 09:37:11 -0500

    initial add

--- prediction_analyses/singularity_analyses/mk_singularity_script.py
+++ prediction_analyses/singularity_analyses/mk_singularity_script.py
@@ -1,9 +1,6 @@
-with open('baseline_sing.sh','w') as f:
+njobs=8
+container='/work/01329/poldrack/stampede2/singularity_images/selfregulation-2017-10-10-646e0b351ab0.img'
+for t in ['baseline','task','survey']:
+  with open('%s_sing.sh'%t,'w') as f:
     for i in range(100):
-       f.write("singularity run -e /work/01329/poldrack/stampede2/singularity_images/selfregulation-2017-10-09-6c180963393a.img /workdir/Self_Regulation_Ontology/prediction_analyses/behav_prediction.py -d baseline -r /scratch/01329/poldrack/SRO -j 8\n") 
-with open('survey_sing.sh','w') as f:
-    for i in range(100):
-       f.write("singularity run -e /work/01329/poldrack/stampede2/singularity_images/selfregulation-2017-10-09-6c180963393a.img /workdir/Self_Regulation_Ontology/prediction_analyses/behav_prediction.py -d survey -r /scratch/01329/poldrack/SRO -j 8\n") 
-with open('task_sing.sh','w') as f:
-    for i in range(100):
-       f.write("singularity run -e /work/01329/poldrack/stampede2/singularity_images/selfregulation-2017-10-09-6c180963393a.img /workdir/Self_Regulation_Ontology/prediction_analyses/behav_prediction.py -d task -r /scratch/01329/poldrack/SRO -j 8\n") 
+       f.write("singularity run -e %s /workdir/Self_Regulation_Ontology/prediction_analyses/behav_prediction.py -d %s -r /scratch/01329/poldrack/SRO -j %d\n"%(container,t,njobs)) 
