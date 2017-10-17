480ef45fcf4bd07fb2c7befd4c605a07d7148a02
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-10-17 12:19:34 -0700

    updated batch files for sherlock 2 and new release

--- batch_files/retest/calculate_retest_exp_DVs.py
+++ batch_files/retest/calculate_retest_exp_DVs.py
@@ -4,7 +4,7 @@ from expanalysis.experiments.processing import get_exp_DVs
 from os import path
 import pandas as pd
 
-data_dir='/scratch/users/zenkavi/Self_Regulation_Ontology/Data/Retest_02-11-2017/Local'
+data_dir='/scratch/users/zenkavi/Self_Regulation_Ontology/Data/Retest_03-21-2017/Local'
 
 #parse arguments
 exp_id = sys.argv[1]
@@ -14,11 +14,11 @@ data = sys.argv[2]
 #else:
  #   out_dir = data_dir
 
-out_dir = '/scratch/users/zenkavi/Self_Regulation_Ontology/Data/Retest_02-11-2017/batch_output'
+out_dir = '/scratch/users/zenkavi/Self_Regulation_Ontology/Data/Retest_03-21-2017/batch_output'
 
 #discovery
 #load Data
-dataset = pd.read_json(path.join(data_dir,'mturk_' + data + '_data_manual_post.json'))
+dataset = pd.read_json(path.join(data_dir,'mturk_' + data + '_data_post.json'))
 
 #calculate DVs
 DV_df, valence_df, description = get_exp_DVs(dataset, exp_id, use_group_fun = True)
@@ -26,4 +26,3 @@ if not DV_df is None:
     DV_df.to_json(path.join(out_dir, exp_id + '_' + data + '_DV.json'))
     valence_df.to_json(path.join(out_dir, exp_id + '_' + data + '_DV_valence.json'))
 print('completed %s %s' % (data, exp_id))
-
