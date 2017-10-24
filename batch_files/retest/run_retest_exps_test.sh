f98288c0c9ee5aee23adfd9d87cfa1bd5346aa87
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-10-24 16:47:42 -0700

    changed shell script for sherlock 1

--- batch_files/retest/run_retest_exps_test.sh
+++ batch_files/retest/run_retest_exps_test.sh
@@ -1,6 +1,6 @@
 set -e
 for exp_id in digit_span
 do
-sed "s/{EXP_ID}/$exp_id/g" calculate_retest_DVs.batch | sbatch
+sed "s/{EXP_ID}/$exp_id/g" calculate_retest_DVs.batch | sbatch -p russpold
 done
 
