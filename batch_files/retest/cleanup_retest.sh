cce41514e2577811a45d44270b5137246edcf996
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-10-27 15:09:04 -0700

    fixed the path in the cleanup script

--- batch_files/retest/cleanup_retest.sh
+++ batch_files/retest/cleanup_retest.sh
@@ -1,7 +1,7 @@
-mkdir ../../Data/Retest_09-27-2017/batch_output/hddm_model
+mkdir ../../Data/Retest_09-27-2017/batch_output/hddm_models
 mkdir ../../Data/Retest_09-27-2017/batch_output/hddm_data
-mv *.model hddm_models
-mv *.db hddm_models
-mv *pcc.csv hddm_models
-mv *_data.csv hddm_data
+mv *.model ../../Data/Retest_09-27-2017/batch_output/hddm_models
+mv *.db ../../Data/Retest_09-27-2017/batch_output/hddm_models
+mv *pcc.csv ../../Data/Retest_09-27-2017/batch_output/hddm_models
+mv *_data.csv ../../Data/Retest_09-27-2017/batch_output/hddm_data
 python concatenate_retest_DVs.py
