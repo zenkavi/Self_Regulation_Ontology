d206916de8c41fb463e881077eceb6bfc1291a8b
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-11-02 01:17:02 -0700

    debugging bootstrapping

--- batch_files/retest/run_bootstrap_retest.sh
+++ batch_files/retest/run_bootstrap_retest.sh
@@ -1,4 +1,4 @@
 while read VAR_ID
 do
     sed "s/{VAR_ID}/$var_id/g" bootstrap_retest.batch | sbatch -p russpold
-done < head -n 1 /oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data/Retest_09-27-2017/variables_exhaustive.csv | sed s/,/'\n'/g
+done < $(head -n 1 /oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data/Retest_09-27-2017/variables_exhaustive.csv | sed s/,/'\n'/g)
