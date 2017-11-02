4c1480dee4bb5e603fa1724c9d75858ac5633c4a
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-11-02 14:10:04 -0700

    debugging bootstrap

--- batch_files/retest/run_bootstrap_retest.sh
+++ batch_files/retest/run_bootstrap_retest.sh
@@ -1,4 +1,3 @@
-while read var_id
-do
-    sed "s/{VAR_ID}/$var_id/g" bootstrap_retest.batch | sbatch -p russpold
-done < variables_exhaustive_names_test.txt
+while read var_id; do
+  sed "s/{VAR_ID}/$var_id/g" bootstrap_retest.batch | sbatch -p russpold
+done <variables_exhaustive_names.txt
