c067589805c94ba4b000fbc299931c6c7e958fc5
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-11-09 14:12:05 -0800

    fixed bootstrap concat

--- batch_files/retest/concatenate_bootstrap.sh
+++ batch_files/retest/concatenate_bootstrap.sh
@@ -1,6 +1,6 @@
 cd /oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data/Retest_09-27-2017/batch_output/bootstrap_output
-cat *.csv > ../Local/bootstrap_merged.csv
-cd ../Local
+cat *.csv > ../../Local/bootstrap_merged.csv
+cd ../../Local
 awk '!a[$0]++' bootstrap_merged.csv > bootstrap_merged_clean.csv
 rm bootstrap_merged.csv
 mv bootstrap_merged_clean.csv ./boostrap_merged.csv
