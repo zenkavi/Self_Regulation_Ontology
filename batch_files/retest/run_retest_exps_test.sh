18fa7a85936c67972be98dc1766595b5c3b04006
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-11-10 08:49:17 -0800

    shell script to run missing dvs

--- batch_files/retest/run_retest_exps_test.sh
+++ batch_files/retest/run_retest_exps_test.sh
@@ -1,5 +1,5 @@
 set -e
-for exp_id in eating_survey five_facet_mindfulness_survey
+for exp_id in dot_pattern_expectancy two_stage_decision
 do
 sed "s/{EXP_ID}/$exp_id/g" calculate_retest_DVs.batch | sbatch -p russpold
 done
