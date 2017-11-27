0dc1b21eb0553e5f9b2d343225da072807d07592
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-11-26 17:22:35 -0800

    changed paths for new release

--- batch_files/retest/concatenate_bootstrap.sh
+++ batch_files/retest/concatenate_bootstrap.sh
@@ -1,4 +1,4 @@
-cd /oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data/Retest_09-27-2017/batch_output/bootstrap_output
+cd /oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data/Retest_11-27-2017/batch_output/bootstrap_output
 cat *.csv > ../../Local/bootstrap_merged.csv
 cd ../../Local
 awk '!a[$0]++' bootstrap_merged.csv > bootstrap_merged_clean.csv
