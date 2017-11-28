fedb771e3f883c085fee76f1e6cb08736d7ce7e9
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-11-27 21:39:04 -0800

    debug

--- batch_files/retest/run_retest_exps_test.sh
+++ batch_files/retest/run_retest_exps_test.sh
@@ -1,5 +1,5 @@
 set -e
-for exp_id in dot_pattern_expectancy two_stage_decision
+for exp_id in alcohol_drugs_survey bickel_titrator
 do
 sed "s/{EXP_ID}/$exp_id/g" calculate_retest_DVs.batch | sbatch -p russpold
 done
