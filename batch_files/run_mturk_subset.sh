17e73a5403c20443d746c19b9c871f7a04aced6f
Ian <ianeisenberg90@gmail.com>
2017-12-03 16:53:46 -0800

    corrected subset typo

--- batch_files/run_mturk_subset.sh
+++ batch_files/run_mturk_subset.sh
@@ -1,5 +1,5 @@
 set -e
 for exp_id in stroop 
 do
-sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUIBSET}/mturk_complete/g" calculate_exp_DVs.batch | sbatch -p russpold
+sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_complete/g" calculate_exp_DVs.batch | sbatch -p russpold
 done
