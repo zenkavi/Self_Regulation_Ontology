1ae7a3c67d95411d5ba5930355fc901c1f2c317f
Ian <ianeisenberg90@gmail.com>
2017-12-05 15:09:00 -0800

    updated docker

--- misc_scripts/example_docker.sh
+++ misc_scripts/example_docker.sh
@@ -3,8 +3,16 @@ docker run --rm  -ti sro_dataprep
 
 # calculate exp dvs
 data_loc=/home/ian/tmp/
-output=/media/Data/Ian/Experiments/expfactory/Self_Regulation_Ontology/batch_files/container_scripts/output
+output=/media/Data/Ian/Experiments/expfactory/Self_Regulation_Ontology/batch_files/singularity_scripts/output
 docker run --rm  \
 --mount type=bind,src=$data_loc,dst=/Data \
 --mount type=bind,src=$output,dst=/output \
--ti sro batch_files/calculate_exp_DVs.py stim_selective_stop_signal mturk_complete --out_dir /output 
+-ti sro_dataprep batch_files/calculate_exp_DVs.py stim_selective_stop_signal mturk_complete --out_dir /output 
+
+# save data
+data_loc=/home/ian/tmp
+output=/home/ian/tmp
+docker run --rm  \
+--mount type=bind,src=$data_loc,dst=/Data \
+--mount type=bind,src=$output,dst=/SRO/Data \
+-ti sro_dataprep python data_preparation/mturk_save_data.py
--- singularity_images/transfer_image.sh
+++ singularity_images/transfer_image.sh
@@ -1,4 +1,3 @@
 sshfs sherlock:/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data /mnt/temp
-rm -f /mnt/temp/singularity_images/*img
 cp *img /mnt/temp/singularity_images/
 sudo umount /mnt/temp
