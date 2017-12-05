885cb75ca215480e218116a84453b800c9fdfcf7
Ian <ianeisenberg90@gmail.com>
2017-12-05 13:25:22 -0800

    updates to docker

--- docker2singularity_dataprep.sh
+++ docker2singularity_dataprep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build --file Dockerfile_dataprep --rm -t sro_dataprep .
 rm -f singularity_images/*img
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v /home/ian/Experiments/expfactory/Self_Regulation_Ontology/singularity_images:/output --privileged -t --rm singularityware/docker2singularity sro
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v /home/ian/Experiments/expfactory/Self_Regulation_Ontology/singularity_images:/output --privileged -t --rm singularityware/docker2singularity sro_dataprep
 echo Finished Conversion
 cd singularity_images
 bash transfer_image.sh
--- misc_scripts/example_docker.sh
+++ misc_scripts/example_docker.sh
@@ -1,8 +1,4 @@
-docker run --rm  \
---mount type=bind,src=/media/Data/Ian/Experiments/expfactory/Self_Regulation_Ontology/Data,dst=/SRO/Data \
---mount type=bind,src=/media/Data/Ian/Experiments/expfactory/Self_Regulation_Ontology/batch_files/container_scripts/output,dst=/output \
---entrypoint /bin/bash \
--ti sro
+docker run --rm  -ti sro_dataprep
 
 
 # calculate exp dvs
