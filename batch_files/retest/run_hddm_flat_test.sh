8254d6cded6cad3e216fbcce819d46c568c44a27
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-12-06 12:35:03 -0800

    modified hddm flat testing script

--- batch_files/retest/run_hddm_flat_test.sh
+++ batch_files/retest/run_hddm_flat_test.sh
@@ -1,5 +1,5 @@
 set -e
-for exp_id in adaptive_n_back attention_network_task choice_reaction_time directed_forgetting dot_pattern_expectancy local_global_letter motor_selective_stop_signal recent_probes shape_matching simon stim_selective_stop_signal stop_signal stroop threebytwo
+for exp_id in adaptive_n_back
 do
 sed "s/{EXP_ID}/$exp_id/g" calculate_hddm_flat.batch | sbatch -p russpold
 done
