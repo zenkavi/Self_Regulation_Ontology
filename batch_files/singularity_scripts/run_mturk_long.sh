1a5157fc4113568b90adfe8447be5cc44f16c8e9
Ian <ianeisenberg90@gmail.com>
2017-12-05 16:45:19 -0800

    split singularity run scripts

---
+++ batch_files/singularity_scripts/run_mturk_long.sh
@@ -0,0 +1,5 @@
+set -e
+for exp_id in adaptive_n_back choice_reaction_time directed_forgetting dot_pattern_expectancy local_global_letter motor_selective_stop_signal recent_probes shift_task simon stim_selective_stop_signal stop_signal stroop two_stage_decision 
+do
+sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_complete/g" calculate_exp_DVs.batch | sbatch --task 40:00:00
+done
