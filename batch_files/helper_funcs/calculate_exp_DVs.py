9e1d32ec45a459d061c5c9c6d3c3019986d16822
Ian <ianeisenberg90@gmail.com>
2017-12-11 00:35:20 -0800

    updated dv calculate function to reflect parallel hddm

--- batch_files/helper_funcs/calculate_exp_DVs.py
+++ batch_files/helper_funcs/calculate_exp_DVs.py
@@ -19,7 +19,11 @@ parser.add_argument('data')
 parser.add_argument('--out_dir', default=data_dir)
 parser.add_argument('--no_group', action='store_false')
 parser.add_argument('--hddm_model_file', default=None)
-parser.add_argument('--hddm_samples', default=None)
+parser.add_argument('--hddm_samples', default=None, type=int)
+parser.add_argument('--hddm_burn', default=None, type=int)
+parser.add_argument('--hddm_thin', default=None, type=int)
+parser.add_argument('--no_parallel', action='store_false')
+parser.add_argument('--num_cores', default=None, type=int)
 
 args = parser.parse_args()
 
@@ -27,10 +31,13 @@ exp_id = args.exp_id
 data = args.data
 out_dir = args.out_dir
 use_group = args.no_group
+# HDDM variables
 model_file = args.hddm_model_file
 hddm_samples = args.hddm_samples
-if hddm_samples is not None:
-    hddm_samples = int(hddm_samples)
+hddm_burn= args.hddm_burn
+hddm_thin= args.hddm_thin
+parallel = args.no_parallel
+num_cores = args.num_cores
 
 #load Data
 dataset = pd.read_pickle(path.join(data_dir, data + '_data_post.pkl'))
@@ -41,15 +48,19 @@ if hddm_samples is None:
     DV_df, valence_df, description = get_exp_DVs(dataset, exp_id, 
                                                  use_group_fun = use_group,
                                                  outfile = path.join(out_dir, exp_id),
-                                                 loadfile = model_file)
+                                                 loadfile = model_file,
+                                                 parallel=parallel,
+                                                 num_cores=num_cores)
 else:
-    burn = min(hddm_samples//10, 10000)
+    burn = min(hddm_samples//2, 20000)
     DV_df, valence_df, description = get_exp_DVs(dataset, exp_id, 
                                                  use_group_fun = use_group,
                                                  outfile = path.join(out_dir, exp_id),
                                                  loadfile = model_file,
                                                  samples = hddm_samples,
-                                                 burn = burn)
+                                                 burn = burn,
+                                                 parallel=parallel,
+                                                 num_cores=num_cores)
     
 if not DV_df is None:
     DV_df.to_json(path.join(out_dir, exp_id + '_' + data + '_DV.json'))
