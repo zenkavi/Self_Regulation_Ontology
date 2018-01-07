03c1d42943c92ecbeb455ea012a05102918670d9
Ian <IanEisenberg@gmail.com>
2018-01-06 17:10:08 -0800

    updated run scripts

--- batch_files/singularity_scripts/run_mturk_subset.sh
+++ batch_files/singularity_scripts/run_mturk_subset.sh
@@ -1,5 +1,6 @@
 set -e
-for exp_id in simon
+for exp_id in adaptive_n_back stop_signal threebytwo
 do
-sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_complete/g" calculate_exp_DVs.batch | sbatch --time=20:00:00 -p russpold
+    sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_complete/g" calculate_exp_DVs.batch | sbatch --time=168:00:00 -p russpold
 done
+
