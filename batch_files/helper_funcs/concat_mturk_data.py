8f01b8817a334609cb232f95da0be0264e7ad784
Ian <IanEisenberg@gmail.com>
2018-01-08 12:54:29 -0800

    update helper fun

--- batch_files/helper_funcs/concat_mturk_data.py
+++ batch_files/helper_funcs/concat_mturk_data.py
@@ -8,8 +8,18 @@ try:
 except Exception:
     data_dir=path.join(get_info('base_directory'),'Data')
 
+# concatenate discovery and validation data into one complete
 discovery = pd.read_pickle(path.join(data_dir, 'mturk_discovery_data_post.pkl'))
 validation = pd.read_pickle(path.join(data_dir, 'mturk_validation_data_post.pkl'))
 
 complete = pd.concat([discovery, validation])
 complete.to_pickle(path.join(data_dir, 'mturk_complete_data_post.pkl'))
+
+# separate complete into two data subsets for particularly memory intensive analyses (DDM)
+
+workers = data.worker_id.unique()
+mid = len(workers)//2
+subset1 = data.query('worker_id in %s' % list(workers)[:mid])
+subset2 = data.query('worker_id in %s' % list(workers)[mid:])
+subset1.to_pickle(path.join(data_dir, 'mturk_complete_subset1_data_post.pkl'))
+subset2.to_pickle(path.join(data_dir, 'mturk_complete_subset2_data_post.pkl'))
