589fc05471b082bd0811722863a1ed527446957d
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-01-20 20:01:30 -0800

    path corrections

--- data_preparation/retest/retest_save_hddm_refits.py
+++ data_preparation/retest/retest_save_hddm_refits.py
@@ -1,7 +1,8 @@
 from os import path
 import pandas as pd
 
-data_dir = path.join('/oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data','Retest_01-17-2018/Local')
+data_dir = '/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_retest_output/hddm_refits/'
+out_dir = '/oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data/Retest_01-17-2018'
 
 #Read in DVs and valence
 label = 'hddm_refit'
@@ -12,9 +13,9 @@ readme_lines = []
 
 #drop na columns
 DV_df.dropna(axis = 1, how = 'all', inplace = True)
-DV_df.to_csv(path.join(data_dir, 'hddm_refits_exhaustive.csv'))
+DV_df.to_csv(path.join(out_dir, 'hddm_refits_exhaustive.csv'))
 readme_lines += ["hddm_refits_exhaustive.csv: all variables for hddm's for retest subjects' t1 data\n\n"]
 
-readme = open(path.join(data_dir, "README.txt"), "a")
+readme = open(path.join(out_dir, "README.txt"), "a")
 readme.writelines(readme_lines)
 readme.close()
