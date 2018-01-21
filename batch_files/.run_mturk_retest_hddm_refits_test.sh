a2cfe8c4fa2cae51710dce8153b3db75d17f7a78
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-01-20 17:44:54 -0800

    fixing hddm refit

--- batch_files/.run_mturk_retest_hddm_refits_test.sh
+++ batch_files/.run_mturk_retest_hddm_refits_test.sh
@@ -1,5 +1,5 @@
 set -e
-for exp_id in adaptive_n_back directed_forgetting choice_reaction_time local_global_letter
+for exp_id in adaptive_n_back choice_reaction_time local_global_letter stroop
 do
 sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_retest/g" calculate_hddm_refits.batch | sbatch 
 done
