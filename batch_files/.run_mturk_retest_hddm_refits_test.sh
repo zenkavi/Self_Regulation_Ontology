c39fecd3ed393caf714e64b701ab0d14815f7b6e
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-01-22 10:22:52 -0800

    hddm fix

--- batch_files/.run_mturk_retest_hddm_refits_test.sh
+++ batch_files/.run_mturk_retest_hddm_refits_test.sh
@@ -1,5 +1,5 @@
 set -e
-for exp_id in attention_network_task dot_pattern_expectancy
+for exp_id in attention_network_task
 do
 sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_retest/g" calculate_hddm_refits.batch | sbatch 
 done
