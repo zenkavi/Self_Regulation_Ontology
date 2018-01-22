1c0fd28a6209f3f19ff8fa1745ada6aa772ea2a8
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-01-22 10:18:25 -0800

    fixed time out  for hddm refits

--- batch_files/.run_mturk_retest_hddm_refits_test.sh
+++ batch_files/.run_mturk_retest_hddm_refits_test.sh
@@ -1,5 +1,5 @@
 set -e
-for exp_id in adaptive_n_back
+for exp_id in attention_network_task dot_pattern_expectancy
 do
 sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_retest/g" calculate_hddm_refits.batch | sbatch 
 done
