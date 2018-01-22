bd74b078d2e8d58d64adb510025ca1292eab187e
Ian <IanEisenberg@gmail.com>
2018-01-21 22:35:22 -0800

    new data release

--- batch_files/run_mturk_subset.sh
+++ batch_files/run_mturk_subset.sh
@@ -1,7 +1,7 @@
 set -e
-for exp_id in adaptive_n_back stop_signal threebytwo
+for exp_id in stroop
 do
-    sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_complete/g" calculate_exp_DVs.batch | sbatch --time=168:00:00
+    sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_retest/g" calculate_exp_DVs.batch | sbatch --time=168:00:00
  
     #sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_complete_subset1/g" $HOME/Self_Regulation_Ontology/batch_files/calculate_exp_DVs.batch | sbatch --time=168:00:00
     #sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_complete_subset2/g" $HOME/Self_Regulation_Ontology/batch_files/calculate_exp_DVs.batch | sbatch --time=168:00:00
