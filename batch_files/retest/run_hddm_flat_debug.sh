1ffdc69eb49fe950027331cf2e9e955c862347e9
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-01-27 11:39:08 -0800

    added script to extract t1 task data and cleaned up retest dir

--- batch_files/retest/run_hddm_flat_debug.sh
+++
@@ -1,5 +0,0 @@
-set -e
-for exp_id in adaptive_n_back 
-do
-sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{DATA_PATH}/\/oak\/stanford\/groups\/russpold\/users\/zenkavi\/Self_Regulation_Ontology\/Data\/Retest_01\-23\-2018\/Individual_Measures\//g" -e "s/{SUBSET}/retest/g" calculate_hddm_flat.batch | sbatch -p russpold
-done
