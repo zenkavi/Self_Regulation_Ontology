5e772790326636a2d5f99cc6ba288eb57cdb52ec
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-01-29 16:23:55 -0800

    testing script for hddm parallel concatenation

--- batch_files/retest/test.sh
+++ batch_files/retest/test.sh
@@ -1,9 +1,9 @@
-cd /oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_retest_output/hddm_flat/subject_fits
 set -e
 for exp_id in choice_reaction_time recent_probes
 do
 	for subset in _retest _t1
     do
+    cd /oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_retest_output/hddm_flat/subject_fits
     sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/$subset/g" find . -type f -name "${EXP_ID}${SUBSET}_s*hddm_flat.csv" -exec cat > "../${EXP_ID}${SUBSET}_hddm_flat.csv" {} \;
     awk '!a[$0]++' "../${EXP_ID}${SUBSET}_hddm_flat.csv" > "../${EXP_ID}${SUBSET}_hddm_flat_clean.csv"
     rm ../${EXP_ID}${SUBSET}_hddm_flat.csv
