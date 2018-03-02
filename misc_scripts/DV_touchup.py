c2c2b74147eef268193513dc99c92c627ba1db4f
Ian <ianeisenberg90@gmail.com>
2018-03-02 14:08:31 -0800

    updated docker

--- misc_scripts/DV_touchup.py
+++ misc_scripts/DV_touchup.py
@@ -5,12 +5,12 @@ Created on Wed Feb 28 12:56:41 2018
 
 @author: ian
 """
-# fix threebytwo
+import numpy as np
 import pandas as pd
+
+# fix threebytwo
 behav_path = '/mnt/temp/mturk_retest_output/threebytwo_mturk_retest_DV.json'
-
 dvs = pd.read_json(behav_path)
-
 param = 'drift'
 dvs['task_switch_cost_hddm_' + param] = dvs['hddm_' + param + '_task_switch'] - dvs['hddm_' + param + '_task_stay']
 dvs.to_json(behav_path)
@@ -49,16 +49,27 @@ dvs.drop(['condition_sensitivity_hddm_drift', 'condition_sensitivity_hddm_thresh
 dvs.to_json(behav_path)
 
 # larger update
-fullDV_loc = '/mnt/temp/mturk_complete_DV.json'
-fullvalence_loc = '/mnt/temp/mturk_complete_DV_valence.json'
-dv = pd.read_json(fullDV_loc)
-valence = pd.read_json(fullvalence_loc)
+
 
 # update
-updateDV = '/mnt/temp/mturk_complete_fix/mturk_complete_DV.json'
-updatevalence = '/mnt/temp/mturk_complete_fix/mturk_complete_DV_valence.json'
-dv_fix = pd.read_json(updateDV)
-valence_fix = pd.read_json(updatevalence)
-
-
-overlap = pd.concat([dv[dv_fix.columns], dv_fix])
+for subset in ['mturk_complete', 'mturk_retest']:
+    fullDV_loc = '/mnt/OAK/%s_DV.json' % subset
+    fullvalence_loc = '/mnt/OAK/%s_DV_valence.json' % subset
+    dv = pd.read_json(fullDV_loc)
+    valence = pd.read_json(fullvalence_loc)
+    
+    updateDV = '/mnt/OAK/%s_fix/%s_DV.json' % (subset, subset)
+    updatevalence = '/mnt/OAK/%s_fix/%s_DV_valence.json' % (subset, subset)
+    dv_fix = pd.read_json(updateDV)
+    valence_fix = pd.read_json(updatevalence)
+    
+    # add non-overlapping columns
+    overlapping_columns = list(set(dv_fix.columns) & set(dv.columns))
+    assert np.mean(np.mean(dv_fix[overlapping_columns] - dv[overlapping_columns])) == 0
+    
+    nonoverlapping_columns = list(set(dv_fix.columns) - set(dv.columns))
+    dv[nonoverlapping_columns] = dv_fix[nonoverlapping_columns]
+    valence[nonoverlapping_columns] = valence_fix[nonoverlapping_columns]
+    
+    dv.to_json(fullDV_loc)
+    valence.to_json(fullvalence_loc)
