12a7f9fc4acbf905432233d8fe71b9797d2136d2
ian <ianeisenberg90@gmail.com>
2018-03-08 14:28:06 -0800

    update learning task replication plots

--- replication/learning_tasks.py
+++ replication/learning_tasks.py
@@ -16,23 +16,21 @@ two_stage_df = get_behav_data(file='Individual_Measures/two_stage_decision.csv.g
 # subset two subjects who passed quality control
 successful_two_stage = data.filter(regex='two_stage').dropna(how='any').index
 two_stage_df = two_stage_df.query('worker_id in %s' % list(successful_two_stage))
-two_stage_df = two_stage_df.query('rt_first != -1 and feedback_last != -1')
-# plot
+two_stage_df = two_stage_df.query('rt_first != -1 and feedback_last in [0,1]')
 colors = sns.hls_palette(2)
-plot_df = two_stage_df.loc[:, ['stage_transition_last', 'feedback_last', 'switch']]
+plot_df = (1-two_stage_df.groupby(['worker_id','stage_transition_last','feedback_last']).switch.mean()).reset_index()
 plot_df.feedback_last = plot_df.feedback_last.replace({0:'Unrewarded', 1:'Rewarded'})
 plot_df.stage_transition_last = \
     plot_df.stage_transition_last.replace({'infrequent':'Rare', 'frequent':'Common'})
-plot_df.insert(0, 'stay', 1-plot_df.switch)
 f = plt.figure(figsize=(12,8))
-sns.barplot(x='feedback_last', y='stay', hue='stage_transition_last', 
+sns.barplot(x='feedback_last', y='switch', hue='stage_transition_last', 
             data=plot_df, 
             order=['Rewarded', 'Unrewarded'],
             hue_order=['Common', 'Rare'],
             palette=colors)
 plt.xlabel('')
-plt.ylabel('Stay Probability')
-plt.title('Two Step Task', y=1.04)
+plt.ylabel('Stay Probability', fontsize=24)
+plt.title('Two Step Task', y=1.04, fontsize=30)
 plt.tick_params(labelsize=20)
 plt.ylim([.5,1])
 ax = plt.gca()
@@ -50,14 +48,16 @@ shift_df = get_behav_data(file='Individual_Measures/shift_task.csv.gz')
 successful_shift = data.filter(regex='shift').dropna(how='any').index
 shift_df = shift_df.query('worker_id in %s' % list(successful_shift))
 shift_df = shift_df.query('rt != -1')
+shift_df = shift_df.groupby(['worker_id','trials_since_switch']).correct.mean().reset_index()
 # plot
 f = plt.figure(figsize=(14,8))
 sns.pointplot('trials_since_switch', 'correct', data=shift_df)
-plt.xticks(range(25), range(25))
-plt.xlabel('Trials Since Switch')
-plt.ylabel('Percent Correct')
-plt.title('Shift Task', y=1.04)
+plt.xticks(range(0,25,5), range(0,25,5))
+plt.xlabel('Trials After Change-Point', fontsize=24)
+plt.ylabel('Percent Correct', fontsize= 24)
+plt.title('Shift Task', y=1.04, fontsize=30)
 plt.tick_params(labelsize=20)
 save_dir = path.join(base_dir, 'Results', 'replication', 'Plots', 'shift_task_replication.%s' % ext)
 f.savefig(save_dir, dpi=300, bbox_inches='tight')
 plt.close()
+# plot
