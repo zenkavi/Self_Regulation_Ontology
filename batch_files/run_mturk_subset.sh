456a6de1bd7d31c31e41dafa1e09bbe44e544268
Ian <IanEisenberg@gmail.com>
2018-03-24 15:58:41 -0700

    updates to batch files

--- batch_files/run_mturk_subset.sh
+++ batch_files/run_mturk_subset.sh
@@ -2,7 +2,7 @@ set -e
 #for exp_id in directed_forgetting local_global_letter shape_matching stop_signal threebytwo 
 for exp_id in motor_selective_stop_signal
 do
-    sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_complete/g" calculate_exp_DVs.batch | sbatch --time=120:00:00
-    sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_retest/g" calculate_exp_DVs.batch | sbatch --time=120:00:00
+    sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_complete/g" calculate_exp_DVs.batch | sbatch --time=168:00:00
+    #sed -e "s/{EXP_ID}/$exp_id/g" -e "s/{SUBSET}/mturk_retest/g" calculate_exp_DVs.batch | sbatch --time=168:00:00
 done
 
