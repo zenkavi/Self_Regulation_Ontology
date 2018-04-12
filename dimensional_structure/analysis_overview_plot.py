1dadaf4328b50f1c8dc9692b111717b330305604
ian <ianeisenberg90@gmail.com>
2018-04-11 20:43:44 -0700

    plot updates

--- dimensional_structure/analysis_overview_plot.py
+++ dimensional_structure/analysis_overview_plot.py
@@ -21,11 +21,11 @@ nfactors = out.shape[1]
 task_subset = pd.concat([
     out.filter(regex='choice_reaction_time', axis=0),
     out.filter(regex='^stop_signal\.(hddm|SSRT)', axis=0)[1:5]])
-
 task_subset_data = data.loc[:, task_subset.index]
+task_variables = list(task_subset.index)
 
 # Ridiculous analysis overview plot
-colors = [[.7, 0.0, 0.0], 
+colors = [[.8, 0.62, 0.0], 
           [0.0, 0.5, 0.0], 
           [0.0, 0.75, 0.75], 
           [0.75, 0.0, 0.75]]
@@ -38,26 +38,26 @@ basefont = 6
 basemarker = 40
 basewidth = .6
 
-#f = plt.figure(figsize=(12, 12))
-#basefont = 16
-#basemarker = 220
-#basewidth = 2
+f = plt.figure(figsize=(12, 12))
+basefont = 16
+basemarker = 220
+basewidth = 1.5
 
 
 participant_ax1 = f.add_axes([.25,.555,.28,.16]) 
 participant_ax2 = f.add_axes([.25,.75,.28,.2]) 
 
-loading_ax1 = f.add_axes([.625,.555,.25,.146]) 
+loading_ax1 = f.add_axes([.625,.55,.25,.162]) 
 loading_ax2 = f.add_axes([.625,.75,.25,.189]) 
 
-participant_distance = f.add_axes([.3,.32,.16,.16]) 
-loading_distance = f.add_axes([.675,.32,.16,.16]) 
+participant_distance = f.add_axes([.3,.29,.16,.16]) 
+loading_distance = f.add_axes([.675,.29,.16,.16]) 
 #participant_distance.axis('off'); loading_distance.axis('off')
-participant_mds = f.add_axes([.25,0,.25,.25]) 
-loading_mds = f.add_axes([.625,0,.25,.25]) 
+participant_mds = f.add_axes([.25,-.02,.25,.25]) 
+loading_mds = f.add_axes([.625,-.02,.25,.25]) 
 # color bars for heatmaps
 cbar_ax = f.add_axes([.92,.6,.03,.3]) 
-cbar_ax2 = f.add_axes([.86,.34,.02,.12]) 
+cbar_ax2 = f.add_axes([.86,.31,.02,.12]) 
 # set background
 back = f.add_axes([0,0,1,1])
 back.axis('off')
@@ -125,7 +125,19 @@ for task_i in range(len(tasks)):
     cbar_ax.tick_params(labelsize=basefont)
     for i in range(1,loading_data.shape[0]):
         loading_axes[task_i].hlines(i, -.2, 6.1, color='white', linewidth=basewidth*3)
-    
+    # add boxes
+    for i, t in enumerate(tick_names[::-1]):
+        box_color = color_lookup[t]
+        box_pos = [-.15, i+.3]
+        loading_axes[task_i].add_patch(Rectangle(box_pos, 
+                    width=.15, height=.4, zorder=100,
+                    facecolor=box_color, edgecolor=box_color, 
+                    linewidth=basewidth, clip_on=False))
+        loading_axes[task_i].hlines(i+.5, -2, -.5, color=box_color, 
+                    clip_on=False, linewidth=basewidth, linestyle=':')
+        
+
+        
 # ****************************************************************************
 # Distance Matrices
 # ****************************************************************************
@@ -133,12 +145,52 @@ participant_distances = squareform(abs_pdist(data.T))
 participant_distances = results['task'].HCA.results['clustering_input-data']['clustered_df']
 loading_distances = results['task'].HCA.results['clustering_input-EFA5']['clustered_df']
 sns.heatmap(participant_distances, ax=participant_distance,
+            cmap=sns.color_palette('gray', n_colors=100),
             xticklabels=False, yticklabels=False, square=True, cbar=False)
 sns.heatmap(loading_distances, ax=loading_distance,
             xticklabels=False, yticklabels=False, square=True, 
+            cmap=sns.color_palette('gray', n_colors=100),
             cbar_kws={'ticks': [0, .99]}, cbar_ax=cbar_ax2)
 participant_distance.set_ylabel('DV', fontsize=basefont*.875)
 loading_distance.set_ylabel('DV', fontsize=basefont*.875)
+participant_distance.set_title('x,y = DV vector in R^522', fontsize=basefont*.6)
+loading_distance.set_title('x,y = DV vector in R^5', fontsize=basefont*.6)
+
+# plot location of top variables
+# update limits
+pad = 0
+lim = list(participant_distance.get_xlim())
+lim[0]-=pad; lim[1]+=pad
+participant_distance.set_xlim(lim); participant_distance.set_ylim(lim[::-1])
+loading_distance.set_xlim(lim); loading_distance.set_ylim(lim[::-1])
+for label in task_variables:
+    if 'drift' in label:
+        name = 'drift rate'
+    elif 'thresh' in label:
+        name = 'threshold'
+    elif 'non_decision' in label:
+        name = 'non-decision'
+    else:
+        name = 'SSRT'
+    line_color = color_lookup[name]
+    var_index = np.where(participant_distances.index==label)[0][0]+pad
+    # plot pretty colors
+    participant_distance.plot([var_index, var_index], 
+                              lim,
+                              color=line_color)
+    participant_distance.plot(lim,
+                              [var_index, var_index],
+                              color=line_color)
+    
+    var_index = np.where(loading_distances.index==label)[0][0]+pad
+    loading_distance.plot([var_index, var_index], 
+                          lim,
+                          color=line_color)
+    loading_distance.plot(lim, 
+                          [var_index, var_index],
+                          color=line_color)
+
+
 # format cbar
 cbar_ax2.set_yticklabels([0, 1])
 cbar_ax2.tick_params(axis='y', length=0)
@@ -190,6 +242,7 @@ loading_mds.set_xticklabels(''); loading_mds.set_yticklabels('')
 loading_mds.tick_params(axis='both', length=0)
 loading_mds.axis('off'); 
 
+"""
 # get example points
 var_locs = []
 subplot_colors=[]
@@ -221,6 +274,7 @@ loading_mds.scatter([v[1][0] for v in var_locs],
                     facecolors='yellow',
                     marker='.',
                     s=basemarker*.7)
+"""
 # ****************************************************************************
 # Text and additional pretty lines
 # ****************************************************************************
@@ -239,41 +293,42 @@ back.text(-.03,.62, 'Choice RT', fontsize=basefont, rotation=0,
 back.text(-.03,.87, 'Stop Signal', fontsize=basefont, rotation=0, 
               horizontalalignment='center', verticalalignment='center')
 # other tasks
-back.text(-.03,.6, 'Bickel Titrator', fontsize=basefont, rotation=0, alpha=.5,
+alpha=.3
+back.text(-.03,.6, 'Bickel Titrator', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.58, 'ART', fontsize=basefont, rotation=0, alpha = .4,
+back.text(-.03,.58, 'ART', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.56, 'ANT', fontsize=basefont, rotation=0, alpha=.3,
+back.text(-.03,.56, 'ANT', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.54, 'Adaptive N-Back', fontsize=basefont, rotation=0, alpha=.2,
-              horizontalalignment='center', verticalalignment='center')
-
-back.text(-.03,.64, 'CCT-Cold', fontsize=basefont, rotation=0, alpha=.5,
-              horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.66, 'CCT-Hot', fontsize=basefont, rotation=0, alpha = .4,
-              horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.68, 'Dietary Decision', fontsize=basefont, rotation=0, alpha=.3,
-              horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.7, 'Digit Span', fontsize=basefont, rotation=0, alpha=.2,
-              horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.72, 'Directed Forgetting', fontsize=basefont, rotation=0, alpha=.1,
+back.text(-.03,.54, 'Adaptive N-Back', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
 
-back.text(-.03,.89, 'Stroop', fontsize=basefont, rotation=0, alpha=.5,
+back.text(-.03,.64, 'CCT-Cold', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.91, 'Three-By-Two', fontsize=basefont, rotation=0, alpha = .4,
+back.text(-.03,.66, 'CCT-Hot', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.93, 'Tower of London', fontsize=basefont, rotation=0, alpha=.3,
+back.text(-.03,.68, 'Dietary Decision', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.85, 'Stim-Selective SS', fontsize=basefont, rotation=0, alpha=.5,
+back.text(-.03,.7, 'Digit Span', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.83, 'Spatial Span', fontsize=basefont, rotation=0, alpha = .4,
+back.text(-.03,.72, 'Directed Forgetting', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.81, 'Simple RT', fontsize=basefont, rotation=0, alpha=.3,
+
+back.text(-.03,.89, 'Stroop', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.79, 'Simon', fontsize=basefont, rotation=0, alpha=.2,
+back.text(-.03,.91, 'Three-By-Two', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
-back.text(-.03,.77, 'Shift Task', fontsize=basefont, rotation=0, alpha=.1,
+back.text(-.03,.93, 'Tower of London', fontsize=basefont, rotation=0, alpha=alpha,
+              horizontalalignment='center', verticalalignment='center')
+back.text(-.03,.85, 'Stim-Selective SS', fontsize=basefont, rotation=0, alpha=alpha,
+              horizontalalignment='center', verticalalignment='center')
+back.text(-.03,.83, 'Spatial Span', fontsize=basefont, rotation=0, alpha=alpha,
+              horizontalalignment='center', verticalalignment='center')
+back.text(-.03,.81, 'Simple RT', fontsize=basefont, rotation=0, alpha=alpha,
+              horizontalalignment='center', verticalalignment='center')
+back.text(-.03,.79, 'Simon', fontsize=basefont, rotation=0, alpha=alpha,
+              horizontalalignment='center', verticalalignment='center')
+back.text(-.03,.77, 'Shift Task', fontsize=basefont, rotation=0, alpha=alpha,
               horizontalalignment='center', verticalalignment='center')
 # add labels 
 cbar_ax.tick_params('y', which='major', pad=basefont*.5)
@@ -289,35 +344,27 @@ loading_ax2.set_xticks(np.arange(.5,5.5,1))
 loading_ax2.set_xticklabels(['Factor %s' % i for i in range(1,nfactors+1)],
                             rotation=45, ha='left', fontsize=basefont*.9)
 # participant box
-back.add_patch(Rectangle((.3385,.56), width=.0115, height=.39, 
+back.add_patch(Rectangle((.3385,.55), width=.0115, height=.4, 
                          facecolor="none", edgecolor='grey', linewidth=basewidth*.75))
 back.text(.3385, .96, 'One Participant', fontsize=basefont*.75, 
           horizontalalignment='center', color='grey')
+
+
 # legend for mds
-back.text(.15, .18, 'DVs (130)', fontsize=basefont, 
+back.text(.13, .18, 'All Task DVs (130)', fontsize=basefont, 
           horizontalalignment='center')
-back.text(.15, .15, 'Threshold', fontsize=basefont, 
+back.text(.13, .15, 'Threshold', fontsize=basefont, 
           horizontalalignment='center', color=color_lookup['threshold'])
-back.text(.15, .125, 'Non-Decision', fontsize=basefont, 
+back.text(.13, .125, 'Non-Decision', fontsize=basefont, 
           horizontalalignment='center', color=color_lookup['non-decision'])
-back.text(.15, .1, 'Drift Rate', fontsize=basefont, 
+back.text(.13, .1, 'Drift Rate', fontsize=basefont, 
           horizontalalignment='center', color=color_lookup['drift rate'])
-back.text(.15, .075, 'SSRT', fontsize=basefont, 
+back.text(.13, .075, 'SSRT', fontsize=basefont, 
           horizontalalignment='center', color=color_lookup['SSRT'])
-back.text(.15, .05, 'Other', fontsize=basefont, 
+back.text(.13, .05, 'Other', fontsize=basefont, 
           horizontalalignment='center', color='grey')
 
 # add connecting lines between participants and loading
-# top task
-back.hlines(.773, .53, .6, alpha=.4,linestyle=':', linewidth=basewidth)
-back.hlines(.82, .53, .6, alpha=.4,linestyle=':', linewidth=basewidth)
-back.hlines(.869, .53, .6, alpha=.4,linestyle=':', linewidth=basewidth)
-back.hlines(.918, .53, .6, alpha=.4,linestyle=':', linewidth=basewidth)
-# bottom task
-back.hlines(.677, .53, .6, alpha=.4,linestyle=':', linewidth=basewidth)
-back.hlines(.626, .53, .6, alpha=.4,linestyle=':', linewidth=basewidth)
-back.hlines(.575, .53, .6, alpha=.4,linestyle=':', linewidth=basewidth)
-
 back.vlines(.565, .3, .42, alpha=.4, linestyle='-', linewidth=basewidth)
 back.vlines(.565, .05, .2, alpha=.4, linestyle='-', linewidth=basewidth)
 
@@ -338,15 +385,17 @@ back.arrow(.53, .725, .05, 0, width=basewidth/200, facecolor='k')
 back.text(.55, .735, 'EFA', fontsize=basefont, 
           horizontalalignment='center')
 # from data to heatmap
-back.arrow(.375, .515, 0, -.01, width=basewidth/250, facecolor='k')
+back.arrow(.375, .515, 0, -.01, width=basewidth/250, edgecolor='k', facecolor='white')
 back.arrow(.75, .515, 0, -.01, width=basewidth/250, facecolor='k')
 back.text(.567, .48, 'Pairwise Distance', fontsize=basefont, 
           horizontalalignment='center')
-back.text(.567, .455, '1-abs(correlation)', fontsize=basefont*.75, 
+back.text(.567, .46, 'Between DVs', fontsize=basefont, 
+          horizontalalignment='center')
+back.text(.567, .435, '1-abs(correlation(x,y))', fontsize=basefont*.7, 
           horizontalalignment='center')
 # from heatmap to MDS
-back.arrow(.375, .31, 0, -.01, width=basewidth/250, facecolor='k')
-back.arrow(.75, .31, 0, -.01, width=basewidth/250, facecolor='k')
+back.arrow(.375, .27, 0, -.01, width=basewidth/250, edgecolor='k', facecolor='white')
+back.arrow(.75, .27, 0, -.01, width=basewidth/250, facecolor='k')
 back.text(.567, .24, 'MDS Projection', fontsize=basefont, 
           horizontalalignment='center')
 
@@ -360,13 +409,14 @@ back.text(.25, .24, 'f', fontsize=basefont*1.56255, fontweight='bold')
 back.text(.85, .24, 'g', fontsize=basefont*1.56255, fontweight='bold')
 
 
-
+"""
 # save
 plot_file = path.dirname(results['task'].plot_dir)
 f.savefig(path.join(plot_file, 'analysis_overview.pdf'), 
                 bbox_inches='tight', 
                 dpi=300)
 
+"""
 """
 # example DDM plot
 np.random.seed(1000)
