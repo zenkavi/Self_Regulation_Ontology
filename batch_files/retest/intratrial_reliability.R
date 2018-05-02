942780c4a791e2b0f741a99c4127ac000c9f0e2d
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-02 09:41:58 -0700

    debug intratrial rel

--- batch_files/retest/intratrial_reliability.R
+++ batch_files/retest/intratrial_reliability.R
@@ -107,37 +107,37 @@ calc_break_dvs = function(df, breaks=c(seq(0, 440, 10)[c(-1, -45)], 439)){
 
 #get dv's for t1 and t2
 #for some reason couldn't get this to word with dplyr but with a loop
-# t1_dvs = t1_tbt %>%
-#   group_by(worker_id) %>%
-#   do(calc_break_dvs(.)) %>%
-#   rename(sub_id = worker_id)
+t1_dvs = t1_tbt %>%
+  group_by(worker_id) %>%
+  do(calc_break_dvs(.)) %>%
+  rename(sub_id = worker_id)
 
-t1_dvs = data.frame()
-for(i in 1:length(unique(t1_tbt$worker_id))){
-  print(i)
-  cur_worker = t1_tbt$worker_id[i]
-  df = t1_tbt %>% filter(worker_id == cur_worker)
-  sub_dvs = calc_break_dvs(df)
-  sub_dvs$sub_id = cur_worker
-  t1_dvs = rbind(t1_dvs, sub_dvs)
-}
+# t1_dvs = data.frame()
+# for(i in 1:length(unique(t1_tbt$worker_id))){
+#   print(i)
+#   cur_worker = unique(t1_tbt$worker_id)[i]
+#   df = t1_tbt %>% filter(worker_id == cur_worker)
+#   sub_dvs = calc_break_dvs(df)
+#   sub_dvs$sub_id = cur_worker
+#   t1_dvs = rbind(t1_dvs, sub_dvs)
+# }
 
 write.csv(t1_dvs, paste0(retest_data_path, 'Local/t1_tbt_dvs.csv'))
 
-# t2_dvs = t2_tbt %>%
-#   group_by(worker_id) %>%
-#   do(calc_break_dvs(.)) %>%
-#   rename(sub_id = worker_id)
-# 
+t2_dvs = t2_tbt %>%
+  group_by(worker_id) %>%
+  do(calc_break_dvs(.)) %>%
+  rename(sub_id = worker_id)
 
-t2_dvs = data.frame()
-for(i in 1:length(unique(t2_tbt$worker_id))){
-  print(i)
-  cur_worker = t2_tbt$worker_id[i]
-  df = t2_tbt %>% filter(worker_id == cur_worker)
-  sub_dvs = calc_break_dvs(df)
-  sub_dvs$sub_id = cur_worker
-  t2_dvs = rbind(t2_dvs, sub_dvs)
-}
+# 
+# t2_dvs = data.frame()
+# for(i in 1:length(unique(t2_tbt$worker_id))){
+#   print(i)
+#   cur_worker = unique(t2_tbt$worker_id[i])
+#   df = t2_tbt %>% filter(worker_id == cur_worker)
+#   sub_dvs = calc_break_dvs(df)
+#   sub_dvs$sub_id = cur_worker
+#   t2_dvs = rbind(t2_dvs, sub_dvs)
+# }
 
 write.csv(t2_dvs, paste0(retest_data_path, 'Local/t2_tbt_dvs.csv'))
