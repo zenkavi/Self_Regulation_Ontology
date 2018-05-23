d8742990970bebe0c5a8f99e1b3663309f9353d2
ian <ianeisenberg90@gmail.com>
2018-05-23 15:14:51 -0700

    updated time analysis to work across measures

--- misc_scripts/time_of_day.py
+++ misc_scripts/time_of_day.py
@@ -4,41 +4,58 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.preprocessing import scale
+import statsmodels.formula.api as smf
 from selfregulation.utils.utils import get_behav_data
 
 def convert_to_time(date_str):
     dt = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
     time = dt.time()
     return time.hour
+
+def plot_time_effects(measure_DVs, melted_DVs, title=None):
+    f, (ax1,ax2) = plt.subplots(1, 2, figsize=(16,8))
+    for name in measure_DVs.columns[:-2]:
+        sns.regplot('hour', name, data=measure_DVs, lowess=True, label=name,
+                    ax=ax1, scatter_kws={'s': 100, 'alpha': .4})
+    ax1.legend()
+    sns.boxplot('split_time', 'value', hue='variable', data=melted, ax=ax2)
+    if title:
+        plt.suptitle(title, fontsize=18)
+    plt.show()
     
+verbose=True
+# load data    
 behav_data = get_behav_data(file='meaningful_variables_imputed.csv')
+measures = np.unique([i.split('.')[0] for i in behav_data.columns])
+time_effects = {}
 
-task_name ='stroop'
-# look at task
-task = get_behav_data(file='Individual_Measures/%s.csv.gz' % task_name)
-task_DVs = behav_data.filter(regex=task_name)
-task_DVs.columns = [i.split('.')[1] for i in task_DVs.columns]
-# scale
-task_DVs = pd.DataFrame(scale(task_DVs), index=task_DVs.index, columns=task_DVs.columns)
-
-finishtimes = task.groupby('worker_id').finishtime.apply(lambda x: np.unique(x)[0])
-daytime = finishtimes.apply(convert_to_time)
-daytime.name='hour'
-task_DVs = pd.concat([task_DVs, daytime], axis=1)
-# add on time split in half and melt
-split_time = task_DVs.hour>task_DVs.hour.median()
-task_DVs = task_DVs.assign(split_time = split_time)
-melted = task_DVs.melt(value_vars=task_DVs.columns[:-2],
-                         id_vars='split_time')
-
-f, (ax1,ax2) = plt.subplots(1, 2, figsize=(16,8))
-for name in task_DVs.columns[:-2]:
-    sns.regplot('hour', name, data=task_DVs, lowess=True, label=name,
-                ax=ax1)
-ax1.legend()
-sns.boxplot('split_time', 'value', hue='variable', data=melted, ax=ax2)
-
-import statsmodels.formula.api as smf
-for name in task_DVs.columns[:-2]:
-    rs = smf.ols(formula = '%s ~ hour' % name, data=task_DVs).fit()
-    print(name, '\nBeta: %s' % rs.params.hour, '\nPvalue: %s\n\n' % rs.pvalues.hour)
+for measure_name in measures[0:2]:
+    measure = get_behav_data(file='Individual_Measures/%s.csv.gz' % measure_name)
+    measure_DVs = behav_data.filter(regex=measure_name)
+    measure_DVs.columns = [i.split('.')[1] for i in measure_DVs.columns]
+    # scale
+    measure_DVs = pd.DataFrame(scale(measure_DVs), index=measure_DVs.index, columns=measure_DVs.columns)
+    
+    finishtimes = measure.groupby('worker_id').finishtime.apply(lambda x: np.unique(x)[0])
+    daytime = finishtimes.apply(convert_to_time)
+    daytime.name='hour'
+    measure_DVs = pd.concat([measure_DVs, daytime], axis=1)
+    # add on time split in half and melt
+    split_time = measure_DVs.hour>measure_DVs.hour.median()
+    measure_DVs = measure_DVs.assign(split_time = split_time)
+    melted = measure_DVs.melt(value_vars=measure_DVs.columns[:-2],
+                             id_vars='split_time')
+    
+    # basic regression
+    time_effects[measure_name] = {}
+    for name in measure_DVs.columns[:-2]:
+        rs = smf.ols(formula = '%s ~ hour' % name, data=measure_DVs).fit()
+        time_effects[measure_name][name] = {'Beta': rs.params.hour,
+                                            'pvalue': rs.pvalues.hour}
+        if verbose:
+            print(name, '\nBeta: %s' % rs.params.hour, 
+                  '\nPvalue: %s\n' % rs.pvalues.hour)
+    if verbose:
+        # plot
+        plot_time_effects(measure_DVs, melted, title=measure_name)
+        input('Press Enter to Continue...')
