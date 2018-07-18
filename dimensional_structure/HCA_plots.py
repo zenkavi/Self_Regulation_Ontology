141131864176ffb1205ecacbf1547a55aa87e229
ian <ianeisenberg90@gmail.com>
2018-07-17 19:11:31 -0700

    changed text sizing for subbranch plot

--- dimensional_structure/HCA_plots.py
+++ dimensional_structure/HCA_plots.py
@@ -161,10 +161,9 @@ def plot_subbranch(cluster_i, tree, loading, cluster_sizes, title=None,
                 vmax=max_val,
                 cmap=colormap,)
     yn, xn = subset_loading.shape
-    xn = max(xn,12) # don't want the x labels too big
-    tick_label_size = min(size*40/xn, size*14/yn)
-    heatmap_ax.tick_params(labelsize=tick_label_size, length=size/2, 
-                           width=size/5, pad=size/2)
+    tick_label_size = size*25/max(yn, 8)
+    heatmap_ax.tick_params(labelsize=tick_label_size, length=size*.5, 
+                           width=size/5, pad=size)
     heatmap_ax.set_yticklabels(heatmap_ax.get_yticklabels(), rotation=0)
     heatmap_ax.set_xticklabels(heatmap_ax.get_xticklabels(), rotation=90)
 
@@ -181,6 +180,7 @@ def plot_subbranch(cluster_i, tree, loading, cluster_sizes, title=None,
     plot_loadings(polar_ax, list(avg_factors), kind='line', offset=.5, 
                   colors=[tree['color_list'][start]],
                   plot_kws={'alpha': .8, 'linewidth': size/2})
+    polar_ax.grid(linewidth=size/8)
     # tick properties of polar plot
     xtick_locs = np.arange(0.0, 2*np.pi, 2*np.pi/len(subset_loading))
     polar_ax.set_xticks(xtick_locs)
@@ -207,7 +207,7 @@ def plot_subbranch(cluster_i, tree, loading, cluster_sizes, title=None,
             text=var, #'this this is a very, very long text',
             va = 'bottom',
             axes = polar_labels,
-            fontsize=size*12/len(labels)##calls ax.add_artist in __init__
+            fontsize=size*12/len(labels)*(12/max([len(l) for l in labels]))**.4##calls ax.add_artist in __init__
         )
         polar_labels.set_xlim([-1,1]); polar_labels.set_ylim([-1,1])
         polar_labels.axis('off') 
