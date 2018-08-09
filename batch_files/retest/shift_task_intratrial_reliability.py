0b29457cb30cf4b53faafd9d4d17057c807a7315
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-08-08 21:24:33 -0700

    cont working on shift task intratrial

--- batch_files/retest/shift_task_intratrial_reliability.py
+++ batch_files/retest/shift_task_intratrial_reliability.py
@@ -1,3 +1,4 @@
+import numpy
 import pandas
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
@@ -23,7 +24,7 @@ def calc_shift_DV(df, dvs = {}):
         rs = smf.glm('correct ~ trials_since_switch+trial_num', data = df, family = sm.families.Binomial()).fit()
         learning_rate = rs.params['trials_since_switch']
         learning_to_learn = rs.params['trial_num']
-    except ValueError:
+    except:
         learning_rate = 'NA'
         learning_to_learn = 'NA'
 
@@ -72,21 +73,30 @@ def calc_shift_DV(df, dvs = {}):
     #nonperseverative_errors
     dvs['nonperseverative_errors'] = {'value': len(df.query("correct==0")) - dvs['perseverative_errors']['value'], 'valence': 'Neg'}
 
-    description = """
-        Shift task has a complicated analysis. Right now just using accuracy and
-        slope of learning after switches (which I'm calling "learning rate")
-        """
-    return dvs, description
+    return dvs
+
+#wrapper function to calculate dvs for each break
+def calc_breaks(df):
+    num_trials = df.shape[0]
+    breaks = numpy.arange(10, num_trials, 10)
+    out_df = pandas.DataFrame()
+    for b in breaks:
+        tmp_df = df.iloc[:b]
+        tmp_dvs = pandas.DataFrame.from_dict(calc_shift_DV(tmp_df))
+        out_df = out_df.append(tmp_dvs, ignore_index = True)
+    return(out_df)
 
 #load requested raw data
 if t == "t1":
-    data = pandas.read_csv()
+    data = pandas.read_csv(test_data_path+'Individual_Measures/shift_task.csv.gz', compression='gzip')
+elif t == "t2":
+    data = pandas.read_csv(retest_data_path+'Individual_Measures/shift_task.csv.gz', compression='gzip')
 
 #extract retest workers from raw data
 retest_workers = ['s198', 's409', 's473', 's286', 's017', 's092', 's403', 's103','s081', 's357', 's291', 's492', 's294', 's145', 's187', 's226','s368', 's425', 's094', 's430', 's376', 's284', 's421', 's034','s233', 's027', 's108', 's089', 's196', 's066', 's374', 's007','s509', 's365', 's305', 's453', 's504', 's161', 's441', 's205','s112', 's218', 's129', 's093', 's180', 's128', 's170', 's510','s502', 's477', 's551', 's307', 's556', 's121', 's237', 's481','s259', 's467', 's163', 's111', 's427', 's508', 's190', 's091','s207', 's484', 's449', 's049', 's336', 's212', 's142', 's313','s369', 's165', 's028', 's216', 's346', 's083', 's391', 's388','s384', 's275', 's442', 's505', 's098', 's456', 's209', 's372','s179', 's168', 's084', 's329', 's373', 's065', 's277', 's026','s011', 's063', 's507', 's005', 's495', 's501', 's032', 's326','s396', 's420', 's469', 's244', 's359', 's110', 's383', 's254','s060', 's339', 's380', 's471', 's206', 's182', 's500', 's314','s285', 's086', 's012', 's097', 's149', 's192', 's173', 's262','s273', 's402', 's015', 's014', 's085', 's489', 's071', 's062','s042', 's009', 's408', 's184', 's106', 's397', 's451', 's269','s295', 's265', 's301', 's082', 's238', 's328', 's334']
 retest_subs_test_data_post = data[data['worker_id'].isin(retest_workers)]
 retest_subs_test_data_post = retest_subs_test_data_post.reset_index()
 
-#loop through each trial subset for each subject to get dvs (double loop)
-
-#write output data
+#groupby subjects and apply the function that would calculate the dv's for each break
+out = data.groupby('worker_id').apply(calc_breaks)
+out.reset_index(level=['worker_id', None])
