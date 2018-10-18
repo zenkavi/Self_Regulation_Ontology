765c1348d8c9e6a57bdb046a93bb3eaa733e4081
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-10-18 13:19:41 -0700

    working on fitstat;

--- batch_files/retest/calculate_hddm_fitstat.py
+++ batch_files/retest/calculate_hddm_fitstat.py
@@ -1,6 +1,8 @@
 from glob import glob
-import hddm
+#import hddm
+import inspect
 from kabuki.utils import concat_models
+import numpy as np
 import os
 from os import path
 import pandas as pd
@@ -9,9 +11,9 @@ import re
 import statsmodels.formula.api as sm
 import sys
 
-from expanalysis.experiments.ddm_utils import load_concat_models
+#from expanalysis.experiments.ddm_utils import load_concat_models
 
-sys.path.append(os.getcwd())
+sys.path.append(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 from post_pred_gen_debug import post_pred_gen
 
 model_dir = sys.argv[1]
@@ -22,6 +24,19 @@ hddm_type = sys.argv[5] #(flat or hierarhical)
 parallel = sys.argv[6]
 sub_id_dir = sys.argv[7]
 samples = sys.argv[8]
+
+#Test for all tasks
+#Test for hierarchical, flat and refit
+
+#model_dir = '/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_retest_output/'
+#task = 'adaptive_n_back'
+#subset = 'retest_'
+#output_dir = '/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_retest_output/hddm_fitstat/'
+#hddm_type = 'hierarchical'
+#parallel = 'yes'
+#sub_id_dir = '/oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data/Retest_03-29-2018/Individual_Measures/'
+#samples = 2
+
 
 ##############################################
 ############ HELPER FUNCTIONS ################
@@ -37,18 +52,32 @@ def get_fitstats(m, samples, groupby=None, append_data = True):
     #Sample from posterior predictive and generate data
     ppc_data_append = post_pred_gen(m, samples = samples, append_data = append_data, groupby=groupby)
     
-    ppc_regression_samples = {}
+    ppc_data_append['log_rt'] = np.log(ppc_data_append.rt)
+    ppc_data_append['log_rt_sampled'] = np.log(ppc_data_append.rt_sampled)
+    
     #This loop should output n*condition*sample regression (e.g. 2*2*100)
-    for (node, sample), sim_data in ppc_data_append.groupby(level=(0,1)):
+    #level 0 = 'node' referring to subject
+    #level 1 = 'sample' referring to condition
+    ppc_regression_samples = {}
+    #for (node, sample), sim_data in ppc_data_append.groupby(level=(0,1)):
+    for (node, sample), sim_data in ppc_data_append.groupby(['node', 'sample']):    
         sample_out = {}
         model = sm.ols(formula='rt ~ rt_sampled', data=sim_data)
+        log_model = sm.ols(formula='log_rt ~ log_rt_sampled', data=sim_data)
         fitted = model.fit()
+        log_fitted = log_model.fit()
         sample_out['int_val'] = fitted.params[0]
         sample_out['int_pval'] = fitted.pvalues[0]
         sample_out['slope_val'] = fitted.params[1]
         sample_out['slope_pval'] = fitted.pvalues[1]
         sample_out['rsq'] = fitted.rsquared
         sample_out['rsq_adj'] = fitted.rsquared_adj
+        sample_out['log_int_val'] = log_fitted.params[0]
+        sample_out['log_int_pval'] = log_fitted.pvalues[0]
+        sample_out['log_slope_val'] = log_fitted.params[1]
+        sample_out['log_slope_pval'] = log_fitted.pvalues[1]
+        sample_out['log_rsq'] = log_fitted.rsquared
+        sample_out['log_rsq_adj'] = log_fitted.rsquared_adj
         ppc_regression_samples.update({node+'_'+str(sample): sample_out})
 
     #Convert sample*subject length dict to dataframe
@@ -56,24 +85,25 @@ def get_fitstats(m, samples, groupby=None, append_data = True):
 
 #Add subj_id and condition columns
     if ppc_regression_samples.index.tolist()[0].find("(") != -1:
-        ppc_regression_samples['condition'] = [s[s.find("(")+1:s.find(")")] for s in ppc_regression_samples.index.tolist()]
+        ppc_regression_samples['condition'] = [s[s.find("_")+1] for s in ppc_regression_samples.index.tolist()]
 
     if ppc_regression_samples.index.tolist()[0].find(".") != -1:
-        ppc_regression_samples['subj_id'] = [s[s.find(".")+1:s.find("_")] for s in ppc_regression_samples.index.tolist()]
+        ppc_regression_samples['subj_id'] = [s[s.find(".")+1:s.find(")")] for s in ppc_regression_samples.index.tolist()]
     else:
         ppc_regression_samples['subj_id'] = 0
                        
-    #Summarize on subject*condition level
-    if 'condition' in ppc_regression_samples.columns:
-        means = ppc_regression_samples.groupby(['condition', 'subj_id']).mean().reset_index()
-        stds = ppc_regression_samples.groupby(['condition', 'subj_id']).std().reset_index()
-        ppc_regression_subj = means.merge(stds, on = ['condition', 'subj_id'], suffixes = ('_mean', '_std'))
-    else:
-        means = ppc_regression_samples.groupby(['subj_id']).mean().reset_index()
-        stds = ppc_regression_samples.groupby(['subj_id']).mean().reset_index()
-        ppc_regression_subj = means.merge(stds, on = ['subj_id'], suffixes = ('_mean', '_std'))    
+    #Summarize on subject*condition level [THIS PART MIGHT NOT BE NECESSARY]
+    #if 'condition' in ppc_regression_samples.columns:
+    #    means = ppc_regression_samples.groupby(['condition', 'subj_id']).mean().reset_index()
+     #   stds = ppc_regression_samples.groupby(['condition', 'subj_id']).std().reset_index()
+      #  ppc_regression_subj = means.merge(stds, on = ['condition', 'subj_id'], suffixes = ('_mean', '_std'))
+    #else:
+     #   means = ppc_regression_samples.groupby(['subj_id']).mean().reset_index()
+      #  stds = ppc_regression_samples.groupby(['subj_id']).mean().reset_index()
+       # ppc_regression_subj = means.merge(stds, on = ['subj_id'], suffixes = ('_mean', '_std'))    
     
-    return(ppc_data_append, ppc_regression_samples, ppc_regression_subj)
+#    return(ppc_data_append, ppc_regression_samples, ppc_regression_subj)
+    return(ppc_regression_samples)
 
 ##############################################
 ############# For Model Loading ##############
@@ -83,11 +113,13 @@ def load_parallel_models(model_path):
     loadfile = sorted(glob(model_path))
     models = []
     for l in loadfile:
-        m = hddm.load(l)
+        #m = hddm.load(l)
         #m.load_db(l, db='pickle')
+        m = pickle.load(open(l, 'rb'))
         models.append(m)
-    m = load_concat_models(models)
-    return m, models
+    #m = load_concat_models(models)
+    #return m, models
+    return models
 
 ##############################################
 ############### Groupby lookup ################
@@ -239,13 +271,6 @@ def get_subids_fun(task=None):
 ############### GET FITSTATS #################
 ##############################################
 
-#Testing parameters for Case 1:
-#model_dir = '/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_retest_output/hddm_flat/subject_fits/'
-#task = 'choice_reaction_time_'
-#subset = 'retest'
-#output_dir = '/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_retest_output/hddm_fitstat/'
-#hddm_type = 'flat'
-
 # Case 1: fitstat for all subjects of flat models (no hierarchy)
 if hddm_type == 'flat':
 ## Strategy: looping through all model files for task, subset
@@ -269,14 +294,6 @@ if hddm_type == 'flat':
 ### Step 7: Output df with task, subset, model type (flat or hierarchical)
     fitstats.to_csv(path.join(output_dir, task+subset+hddm_type+'_fitstats.csv'))
 
-#Testing parameters for Case 2a:
-#model_dir = '/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_retest_output/hddm_refits/'
-#task = 'choice_reaction_time'
-#subset = 'refits_'
-#output_dir = '/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_retest_output/hddm_fitstat/'
-#hddm_type = 'hierarchical'
-#parallel = 'yes'
-#sub_id_dir = '/oak/stanford/groups/russpold/users/zenkavi/Self_Regulation_Ontology/Data/Retest_01-23-2018/t1_data/Individual_Measures'
 
 # Case 2: fitstat for all subjects for hierarchical models
 if hddm_type == 'hierarchical':
@@ -285,13 +302,14 @@ if hddm_type == 'hierarchical':
 ### Step 1a: Concatenate all model outputs from parallelization
         model_path = path.join(model_dir, task+'_parallel_output','*.model')
         loaded_models = load_parallel_models(model_path)
-        m_concat = concat_models(loaded_models[1])
+        m_concat = concat_models(loaded_models)
 ### Step 2a: Get fitstat for all subjects from concatenated model
         fitstats = get_fitstats(m_concat)
 ## Case 2b: without parallelization
     elif parallel == 'no':
 ### Step 1b: Read model in
-        m = hddm.load(path.join(model_dir,task+'.model'))
+        #m = hddm.load(path.join(model_dir,task+'.model'))
+        m = pickle.load(open(path.join(model_dir,task+'.model'), 'rb'))
 ### Step 2b: Get fitstats
         fitstats = get_fitstats(m)
 ### Step 3: Extract sub id from correct df that was used for hddm
@@ -299,9 +317,10 @@ if hddm_type == 'hierarchical':
     sub_df = pd.read_csv(path.join(sub_id_dir, task+'.csv.gz'), compression='gzip')
     subids = subid_fun(sub_df)
 ### Step 4: Change keys in fitstats dic
-    fitstats = dict((subids[key], value) for (key, value) in fitstats.items())
+    #fitstats = dict((subids[key], value) for (key, value) in fitstats.items())
+    fitstats = fitstats.replace({'subj_id': subids})
 ### Step 5: Convert list to df
-    fitstats = pd.DataFrame.from_dict(fitstats, orient='index').rename(index=str, columns={0:"KL"})
+    #fitstats = pd.DataFrame.from_dict(fitstats, orient='index').rename(index=str, columns={0:"KL"})
 ### Step 6: Output df with task, subset, model type (flat or hierarchical)
     fitstats.to_csv(path.join(output_dir, task+ '_'+subset+hddm_type+'_fitstats.csv'))
 
