e2dea5456b3c32687c51047f28180757bd5e405b
Ian <ianeisenberg90@gmail.com>
2018-10-21 22:50:25 -0700

    added utility function to transfer score calculation to new dataset

--- dimensional_structure/EFA_test_retest.py
+++ dimensional_structure/EFA_test_retest.py
@@ -14,51 +14,38 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import scale
 
+from dimensional_structure.utils import transfer_scores
 from selfregulation.utils.data_preparation_utils import (remove_outliers, 
                                                          transform_remove_skew)
 from selfregulation.utils.plot_utils import format_num, place_letter, save_figure
-from selfregulation.utils.r_to_py_utils import get_attr, missForest
+from selfregulation.utils.r_to_py_utils import get_attr, missForest, psychFA
 from selfregulation.utils.utils import get_behav_data
 
-def calc_EFA_retest(results, verbose=True):
-    name = results.ID.split('_')[0].title()
-    data = results.data
-    positive_skewed = [i.replace('.logTr', '') for i in data.columns if ".logTr" in i]
-    negative_skewed = [i.replace('.ReflogTr', '') for i in data.columns if ".ReflogTr" in i]
-    DVs = [i.replace('.logTr','').replace('.ReflogTr','') for i in data.columns]
     
-    # load and clean retest data exactly like original data
+def calc_EFA_retest(results, verbose=True):
+    name = results.ID.split('_')[0].title()    
     retest_data_raw = get_behav_data(dataset=results.dataset.replace('Complete','Retest'),
                                      file='meaningful_variables.csv')
-    shared_ids = set(retest_data_raw.index) & set(data.index)
+    shared_ids = set(retest_data_raw.index) & set(results.data.index)
     retest_data_raw = retest_data_raw.loc[shared_ids, :]
-    retest_data = retest_data_raw.loc[:, DVs]
-    retest_data = remove_outliers(retest_data)
-    retest_data = transform_remove_skew(retest_data,
-                                        positive_skewed=positive_skewed,
-                                        negative_skewed=negative_skewed)
-    retest_data_imputed, error = missForest(retest_data)
-    
+    retest_scores = transfer_scores(retest_data_raw, results)
+    retest_scores.columns = [i+' Retest' for i in retest_scores.columns]
     # scale and perform the factor score transformation
-    scaled_retest = scale(retest_data_imputed)
     EFA = results.EFA
     c = EFA.results['num_factors']
-    scores = EFA.get_scores(c=c).loc[retest_data_imputed.index, :]
-    weights = get_attr(EFA.results['factor_tree_Rout_oblimin'][c], 'weights')
-    retest_scores = pd.DataFrame(scaled_retest.dot(weights),
-                                 index=retest_data_imputed.index,
-                                 columns=[i+' Retest' for i in scores.columns])
+    ref_scores = EFA.get_scores(c=c).loc[retest_data_raw.index, :]
+
     # reorder scores
     reorder_vec = EFA.get_factor_reorder(c)
-    scores = scores.iloc[:, reorder_vec]
+    ref_scores = ref_scores.iloc[:, reorder_vec]
     retest_scores = retest_scores.iloc[:, reorder_vec]
-    combined = pd.concat([scores, retest_scores], axis=1)
-    cross_diag = [combined.corr().iloc[i,i+len(scores.columns)] 
-                    for i in range(len(scores.columns))]
+    combined = pd.concat([ref_scores, retest_scores], axis=1)
+    cross_diag = [combined.corr().iloc[i,i+len(ref_scores.columns)] 
+                    for i in range(len(ref_scores.columns))]
     
     if verbose:
         print('%s, Avg Correlation: %s\n' % (name, format_num(np.mean(cross_diag))))
-        for factor, num in zip(scores.columns, cross_diag):
+        for factor, num in zip(ref_scores.columns, cross_diag):
             print('%s: %s' % (factor, format_num(num)))
     return combined, cross_diag
 
@@ -90,7 +77,6 @@ def calc_EFA_retest_held_out(results, verbose=True):
         scaled_tmp_data = scale(tmp_data_imputed)
         imputed_data[name] = scaled_tmp_data
     
-    from selfregulation.utils.r_to_py_utils import get_attr, psychFA
     # get subjects not in the retest set
     ind_data = orig_data.loc[set(orig_data.index)-shared_ids]
     fa, output = psychFA(ind_data, results.EFA.results['num_factors'], 
