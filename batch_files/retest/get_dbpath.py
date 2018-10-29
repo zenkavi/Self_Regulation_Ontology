137175bc944bd526fff0bad487afca44d2473f0f
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-10-29 15:50:16 -0700

    debug path

--- batch_files/retest/get_dbpath.py
+++ batch_files/retest/get_dbpath.py
@@ -1,11 +1,5 @@
 from sys import path
 
 def get_dbpath():
-    if model_dir is None or task is None:
-        print('Assigning default values for model_dir and task')
-        model_dir = '/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/mturk_complete_output/'
-        task = 'test'
-
-    model_path = path.join(model_dir, task+'_parallel_output')
-    
+    model_path = path.join(model_dir, task+'_parallel_output')  
     return model_path
