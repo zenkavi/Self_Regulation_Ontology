16548abe1c11dec778e3b7460942a8ce4b316559
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-11-02 20:32:59 -0700

    adding kl scripts

---
+++ batch_files/retest/calculate_hddm_kl.R
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+#Usage:
+#Rscript --vanilla calculate_hddm_kl.R ppc_data_dir
+
+data_dir <- args[1]
+
+library(tidyverse)
+library(entropy)
+sem <- function(x) {sd(x, na.rm=T) / sqrt(length(x))}
+
+file_list = list.files(data_dir, pattern = "ppc_data")
+
+for(f in file_list){
+  
+  ppc_data = read.csv(paste0(data_dir, f))
+  
+  kl_fname = gsub('ppc_data', 'kl', f)
+
+  kl_df = ppc_data %>%
+    group_by() %>%
+    mutate(rt = ifelse(rt<0, 0.00000000001, rt),
+           rt_sampled = ifelse(rt_sampled<0, 0.00000000001, rt_sampled)) %>%
+    group_by(node, sample) %>%
+    do(data.frame(kl = KL.plugin(.$rt, .$rt_sampled)))
+  
+  write.csv(kl_df, paste0(data_dir, kl_fname))
+  
+  kl_sum_fname = gsub('kl', 'kl_summary', kl_fname)
+  
+  kl_sum_df = kl_df %>%
+    group_by(node) %>%
+    summarise(mean_kl = mean(kl),
+              sem_kl = sem(kl))
+  
+  write.csv(kl_sum_df, paste0(data_dir, kl_sum_fname))
+}
---
+++ batch_files/retest/run_hddm_kl.sh
@@ -0,0 +1,5 @@
+set -e
+for ppc_data_dir in num_samples_test refit retest_flat retet_hierarchical t1_flat t1_hierarchical
+do
+sed -e "s/{PPC_DATA_DIR}/$ppc_data_dir/g" -e calculate_hddm_kl.batch | sbatch
+done
