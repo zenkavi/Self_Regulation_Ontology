3ad67614eb208cb718caeae6a0b11c7657715afb
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-11-02 21:17:22 -0700

    added print statements to help debugging

--- batch_files/retest/calculate_hddm_kl.R
+++ batch_files/retest/calculate_hddm_kl.R
@@ -12,11 +12,17 @@ sem <- function(x) {sd(x, na.rm=T) / sqrt(length(x))}
 
 file_list = list.files(data_dir, pattern = "ppc_data")
 
+print('Starting loop...')
+
 for(f in file_list){
+  
+  print(paste0('Reading in ', f))
   
   ppc_data = read.csv(paste0(data_dir, f))
   
   kl_fname = gsub('ppc_data', 'kl', f)
+  
+  print(paste0('Processing ppc data for', f))
 
   kl_df = ppc_data %>%
     group_by() %>%
@@ -25,14 +31,20 @@ for(f in file_list){
     group_by(node, sample) %>%
     do(data.frame(kl = KL.plugin(.$rt, .$rt_sampled)))
   
+  print('Wiriting out first file')
+  
   write.csv(kl_df, paste0(data_dir, kl_fname))
   
   kl_sum_fname = gsub('kl', 'kl_summary', kl_fname)
+  
+  print('Summarizing at subject level')
   
   kl_sum_df = kl_df %>%
     group_by(node) %>%
     summarise(mean_kl = mean(kl),
               sem_kl = sem(kl))
+  
+  print('Wiriting out second file')
   
   write.csv(kl_sum_df, paste0(data_dir, kl_sum_fname))
 }
