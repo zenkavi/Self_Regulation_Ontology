19c49d4d0374c391b188d36d75d477d79a64c8a6
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-11-05 07:40:02 -0800

    working on kl

--- batch_files/retest/calculate_hddm_fitstat.py
+++ batch_files/retest/calculate_hddm_fitstat.py
@@ -16,6 +16,8 @@ os.environ['PARALLEL'] = parallel
 sub_id_dir = sys.argv[7]
 samples = int(float(sys.argv[8]))
 
+load_ppc = sys.argv[9]
+
 from glob import glob
 import inspect
 from kabuki.utils import concat_models
@@ -23,6 +25,7 @@ import numpy as np
 import pandas as pd
 import pickle
 import re
+from scipy.stats import entropy
 import statsmodels.formula.api as sm
 sys.path.append(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 from post_pred_gen_debug import post_pred_gen
@@ -37,56 +40,79 @@ from post_pred_gen_debug import post_pred_gen
 ##############################################
 
 # Define helper function to get fitstat
-def get_fitstats(m, samples = samples, groupby=None, append_data = True, output_dir = output_dir, task = task, subset = subset, hddm_type=hddm_type):
+def get_fitstats(m, samples = samples, groupby=None, append_data = True, output_dir = output_dir, task = task, subset = subset, hddm_type=hddm_type, load_ppc = load_ppc):
 
-    #Sample from posterior predictive and generate data
-    ppc_data_append = post_pred_gen(m, samples = samples, append_data = append_data, groupby=groupby)
+    if load_ppc == False:
+    
+        #Sample from posterior predictive and generate data
+        ppc_data_append = post_pred_gen(m, samples = samples, append_data = append_data, groupby=groupby)
+    
+        ppc_data_append.reset_index(inplace=True)
 
-    ppc_data_append.reset_index(inplace=True)
+        if(all(v is not None for v in [output_dir, task, hddm_type]) and subset != 'flat'):
+            if(samples != 500):
+                ppc_data_append.to_csv(path.join(output_dir, task+ '_'+subset+hddm_type+'_ppc_data_' + str(samples) +'.csv'))
+            ppc_data_append.to_csv(path.join(output_dir, task+ '_'+subset+hddm_type+'_ppc_data.csv'))
 
-    if(all(v is not None for v in [output_dir, task, hddm_type]) and subset != 'flat'):
-        if(samples != 500):
-            ppc_data_append.to_csv(path.join(output_dir, task+ '_'+subset+hddm_type+'_ppc_data_' + str(samples) +'.csv'))
-        ppc_data_append.to_csv(path.join(output_dir, task+ '_'+subset+hddm_type+'_ppc_data.csv'))
-
+    if load_ppc == True:
+         ppc_data_append = pd.read_csv(...)
+         
+    
     #Adding 3 to avoid negatives
     ppc_data_append['log_rt'] = np.log(ppc_data_append.rt+3)
     ppc_data_append['log_rt_sampled'] = np.log(ppc_data_append.rt_sampled+3)
+    
+    ppc_data_append['rt'] = np.where(ppc_data_append['rt']<0, 0.00000000001, ppc_data_append['rt'])
+    ppc_data_append['rt_sampled'] = np.where(ppc_data_append['rt_sampled']<0, 0.00000000001, ppc_data_append['rt_sampled'])
 
     #This loop should output n*condition*sample regression (e.g. 2*2*100)
     #level 0 = 'node' referring to subject
-    #level 1 = 'sample' referring to correct vs incorrect (?)
+    #level 1 = 'sample' referring to number of sample from posterior predictive distribution
     #will this work with flat?
-    ppc_regression_samples = {}
-    for (node), sim_data in ppc_data_append.groupby(['node']):
-        sub_out = {}
-        model = sm.ols(formula='rt ~ rt_sampled', data=sim_data)
-        fitted = model.fit()
-        sub_out['int_val'] = fitted.params[0]
-        sub_out['int_pval'] = fitted.pvalues[0]
-        sub_out['slope_val'] = fitted.params[1]
-        sub_out['slope_pval'] = fitted.pvalues[1]
-        sub_out['rsq'] = fitted.rsquared
-        sub_out['rsq_adj'] = fitted.rsquared_adj
-
+    
+    def get_rt_kl(sim_data):
+        return entropy(sim_data['rt'],sim_data['rt_sampled'])
+    
+    def get_r_stat(sim_data):
+        sim_data['rt'] = sim_data.rt.sort_values
+        sim_data['rt_sampled'] = sim_data.rt_sampled.sort_values 
         try:
+            model = sm.ols(formula='rt ~ rt_sampled', data=sim_data)
+            fitted = model.fit()
+            
             log_model = sm.ols(formula='log_rt ~ log_rt_sampled', data=sim_data)
             log_fitted = log_model.fit()
-            sub_out['log_int_val'] = log_fitted.params[0]
-            sub_out['log_int_pval'] = log_fitted.pvalues[0]
-            sub_out['log_slope_val'] = log_fitted.params[1]
-            sub_out['log_slope_pval'] = log_fitted.pvalues[1]
-            sub_out['log_rsq'] = log_fitted.rsquared
-            sub_out['log_rsq_adj'] = log_fitted.rsquared_adj
+            
+            sub_out = pd.DataFrame({'int_val': fitted.params[0],
+                                    'int_pval': fitted.pvalue[0],
+                                    'slope_val': fitted.params[1],
+                                    'slope_pval':fitted.pvalues[1],
+                                    'rsq': fitted.rsquared,
+                                    'rsq_adj': fitted.rsquared_adj,
+                                    'log_int_val': log_fitted.params[0],
+                                    'log_int_pval': log_fitted.pvalue[0],
+                                    'log_slope_val': log_fitted.params[1],
+                                    'log_slope_pval': log_fitted.pvalues[1],
+                                    'log_rsq': log_fitted.rsquared,
+                                    'log_rsq_adj': log_fitted.rsquared_adj},
+            index=[0])
         except:
-            sub_out['log_int_val'] = np.nan
-            sub_out['log_int_pval'] = np.nan
-            sub_out['log_slope_val'] = np.nan
-            sub_out['log_slope_pval'] = np.nan
-            sub_out['log_rsq'] = np.nan
-            sub_out['log_rsq_adj'] = np.nan
-
-        ppc_regression_samples.update({node: sub_out})
+            sub_out = pd.DataFrame({'int_val': np.nan,
+                                    'int_pval': np.nan,
+                                    'slope_val': np.nan,
+                                    'slope_pval': np.nan,
+                                    'rsq': np.nan,
+                                    'rsq_adj': np.nan,
+                                    'log_int_val': np.nan,
+                                    'log_int_pval': np.nan,
+                                    'log_slope_val': np.nan,
+                                    'log_slope_pval': np.nan,
+                                    'log_rsq': np.nan,
+                                    'log_rsq_adj': np.nan},
+                index=[0])
+        return sub_out
+    
+   
 
     #Convert sample*subject length dict to dataframe
     ppc_regression_samples = pd.DataFrame.from_dict(ppc_regression_samples, orient="index")
@@ -109,9 +135,8 @@ def get_fitstats(m, samples = samples, groupby=None, append_data = True, output_
 ############# For Model Loading ##############
 ##############################################
 
-#When db names are stored in path names different that where they actually are loading the model directly doesn't work
-    #One could instead create a temporary copy of the traces.db files in the path where the model object thinks it should be (I did this for the refits because it took longer to find a better solution)
-    #Now instead of this I define the object 'container' which pymc.database.pickle uses to load the db. I also modify pymc.database.pickle as detailed in pickle_debug.py
+#When db names are stored in path names different that where they actually are loading the model using the default pymc.database.pickle directly doesn't work
+#To fix this I modified pymc.database.pickle as detailed in pickle_debug.py. This won't work without the modified pymc.database.pickle
 
 def load_parallel_models(model_path):
     loadfile = sorted(glob(model_path))
@@ -280,28 +305,35 @@ if hddm_type == 'flat':
     fitstats = pd.DataFrame()
 
     ### Step 1: Read model in for a given subject
-    for model in models_list:
-
-        try:
-            m = pickle.load(open(model, 'rb'))
-        except NameError:
-            db_path = model.split('.')[0]+'_traces.db'
-            container = pickle.load(open(db_path,'rb'))
-            file = pd.DataFrame()
-            file.name = model
-            m = pickle.load(open(model, 'rb'))
-
-        ### Step 2: Get fitstat for read in model
-        fitstat = get_fitstats(m)
-
-        ### Step 3: Extract sub id from file name
-        sub_id = re.search(model_dir+task+ '_'+subset+'(.+?)_flat.model', model).group(1)
-
-        ### Step 4: Update flat fitstat dict with sub_id
-        fitstat['sub_id'] = sub_id
-
-        ### Step 5: Add individual output to dict with all subjects
-        fitstats = fitstats.append(fitstat)
+    if load_ppc == False:
+    
+        for model in models_list:
+    
+            try:
+                m = pickle.load(open(model, 'rb'))
+            except NameError:
+                db_path = model.split('.')[0]+'_traces.db'
+                container = pickle.load(open(db_path,'rb'))
+                file = pd.DataFrame()
+                file.name = model
+                m = pickle.load(open(model, 'rb'))
+    
+            ### Step 2: Get fitstat for read in model
+            fitstat = get_fitstats(m)
+    
+            ### Step 3: Extract sub id from file name
+            sub_id = re.search(model_dir+task+ '_'+subset+'(.+?)_flat.model', model).group(1)
+    
+            ### Step 4: Update flat fitstat dict with sub_id
+            fitstat['sub_id'] = sub_id
+    
+            ### Step 5: Add individual output to dict with all subjects
+            fitstats = fitstats.append(fitstat)
+    
+    elif load_ppc == True:
+        
+        ppc_data = pd.read_csv(...)
+        fitstats = fitstats(ppc_data)
 
     ### Step 6: Output df with task, subset, model type (flat or hierarchical)
     if(samples != 500):
@@ -313,35 +345,42 @@ if hddm_type == 'flat':
 # Case 2: fitstat for all subjects for hierarchical models
 if hddm_type == 'hierarchical':
 
-    ## Case 2a: with parallelization
-    if parallel == 'yes':
+    if load_ppc == False:
+    
+        ## Case 2a: with parallelization
+        if parallel == 'yes':
+    
+            ### Step 1a: Concatenate all model outputs from parallelization
+            model_path = path.join(model_dir, task+'_parallel_output','*.model')
+            loaded_models = load_parallel_models(model_path)
+            m_concat = concat_models(loaded_models)
+    
+            ### Step 2a: Get fitstat for all subjects from concatenated model
+            fitstats = get_fitstats(m_concat)
+    
+        ## Case 2b: without parallelization
+        elif parallel == 'no':
+    
+            ### Step 1b: Read model in
+            m = pickle.load(open(path.join(model_dir,task+'.model'), 'rb'))
+    
+            ### Step 2b: Get fitstats
+            fitstats = get_fitstats(m)
+    
+        ### Step 3: Extract sub id from correct df that was used for hddm
+        subid_fun = get_subids_fun(task)
+        sub_df = pd.read_csv(path.join(sub_id_dir, task+'.csv.gz'), compression='gzip')
+        subids = subid_fun(sub_df)
+    
+        ### Step 4: Change keys in fitstats dic
+        fitstats[['subj_id']] = fitstats[['subj_id']].apply(pd.to_numeric, errors='coerce')
+        fitstats = fitstats.replace({'subj_id': subids})
 
-        ### Step 1a: Concatenate all model outputs from parallelization
-        model_path = path.join(model_dir, task+'_parallel_output','*.model')
-        loaded_models = load_parallel_models(model_path)
-        m_concat = concat_models(loaded_models)
-
-        ### Step 2a: Get fitstat for all subjects from concatenated model
-        fitstats = get_fitstats(m_concat)
-
-    ## Case 2b: without parallelization
-    elif parallel == 'no':
-
-        ### Step 1b: Read model in
-        m = pickle.load(open(path.join(model_dir,task+'.model'), 'rb'))
-
-        ### Step 2b: Get fitstats
-        fitstats = get_fitstats(m)
-
-    ### Step 3: Extract sub id from correct df that was used for hddm
-    subid_fun = get_subids_fun(task)
-    sub_df = pd.read_csv(path.join(sub_id_dir, task+'.csv.gz'), compression='gzip')
-    subids = subid_fun(sub_df)
-
-    ### Step 4: Change keys in fitstats dic
-    fitstats[['subj_id']] = fitstats[['subj_id']].apply(pd.to_numeric, errors='coerce')
-    fitstats = fitstats.replace({'subj_id': subids})
-
+    elif load_ppc == True:
+        
+        ppc_data = pd.read_csv(...)
+        fitstats = fitstats(ppc_data)
+    
     ### Step 5: Output df with task, subset, model type (flat or hierarchical)
     if(samples != 500):
         fitstats.to_csv(path.join(output_dir, task+ '_'+subset+hddm_type+'_fitstats_'+ str(samples) +'.csv'))
--- batch_files/retest/run_hddm_kl_test.sh
+++ batch_files/retest/run_hddm_kl_test.sh
@@ -1,5 +1,5 @@
 set -e
-for ppc_data_dir in retet_hierarchical
+for ppc_data_dir in num_samples_test retest_flat t1_flat
 do
 sed -e "s/{PPC_DATA_DIR}/$ppc_data_dir/g" calculate_hddm_kl.batch | sbatch
 done
