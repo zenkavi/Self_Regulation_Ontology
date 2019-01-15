c0ded40a39b4c7c53008f81626626a4455539b2b
Ian <ianeisenberg90@gmail.com>
2019-01-15 13:41:06 -0800

    corrected sherlock location

--- singularity_images/transfer_image.sh
+++ singularity_images/transfer_image.sh
@@ -1,3 +1,3 @@
-scp *img ieisenbe@dtn.sherlock.stanford.edu:/oak/stanford/groups/russpold/users/ieisenbe/Self_Regulation_Ontology/behavioral_data/singularity_images/
+scp *img ieisenbe@dtn.sherlock.stanford.edu:/oak/stanford/groups/russpold/users/ieisenbe/uh2/behavioral_data/singularity_images/
 
 
