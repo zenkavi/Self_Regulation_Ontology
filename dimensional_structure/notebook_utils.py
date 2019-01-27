ef852e54f3d3fb3540ab73f5d6ec253308e56d96
Ian <ianeisenberg90@gmail.com>
2019-01-26 19:41:13 -0800

    added visualization of consensus clustering HCA robustness analysis

--- dimensional_structure/notebook_utils.py
+++ dimensional_structure/notebook_utils.py
@@ -1,5 +1,6 @@
 from matplotlib.colors import to_hex
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -35,14 +36,14 @@ def plot_factor_df(EFA, rotate='oblimin'):
     ]
     
     cm =sns.diverging_palette(220,15,n=200)
-    def color_red_or_green(val):
+    def color_cell(val):
         color = to_hex(cm[int(val*100)+100])
         return 'background-color: %s' % color
 
     
     styler = loadings.style
     styler \
-        .applymap(color_red_or_green) \
+        .applymap(color_cell) \
         .set_properties(**{'max-width': '100px', 'font-size': '0pt', 'border-color': 'white'})\
         .set_precision(2)\
         .set_table_styles(magnify())
@@ -86,3 +87,146 @@ def plot_bootstrap_results(boot_stats):
     ax2.set_ylabel('Standard Deviation of Loading')
 
     plt.subplots_adjust(wspace=.5)
+
+def plot_HCA_AMI(consensus):
+    simulation_AMI_values = consensus.AMI_scores
+    mean_simulation = np.mean(simulation_AMI_values)
+    consensus_AMI = consensus.compare_clusters()
+    f = plt.figure(figsize=(12,8))
+    f = sns.kdeplot(simulation_AMI_values, shade=True, color='grey')
+    ymin, ymax = f.get_ylim()
+    f.set_xlim(0,1)
+    plt.axvline(consensus_AMI, ymin, ymax, linestyle='--')
+    plt.xlabel('Adjusted mutual Information (AMI)')
+    plt.title('AMI vs. Original Clustering')
+    plt.text(consensus_AMI, ymax*.75, 
+             'Consensus\nCluster', ha='center', fontsize=25,
+            color='red')
+    plt.text(mean_simulation, ymax*.05, 
+             'Distribution\nover\nSimulations', ha='center', fontsize=25,
+            color='grey')
+    
+def display_closest_DVs(consensus, n_closest=10):
+    nth = {
+        1: "first",
+        2: "second",
+        3: "third",
+        4: "fourth",
+        5: "fifth",
+        6: "sixth",
+        7: "seventh",
+        8: "eigth",
+        9: "ninth",
+        10: "tenth",
+    }
+    df = consensus.get_consensus_cluster()['distance_df']
+    df.index = format_variable_names(df.index)
+    df.columns = format_variable_names(df.columns)
+
+    sorted_df = pd.DataFrame(data=np.zeros((len(df),n_closest)), index=df.index)
+    sorted_df.columns = [nth[i+1] for i in sorted_df.columns]
+    for name, row in sorted_df.iterrows():
+        closest = 1-df.loc[name].drop(name).sort_values()[:n_closest]
+        closest = ['%s: %s%%' % (i,int(b*100)) for i,b in closest.iteritems()]
+        sorted_df.loc[name] = closest
+        
+    def magnify():
+        return [dict(selector="tr:hover",
+                    props=[("border-top", "2pt solid black"),
+                           ("border-bottom", "2pt solid black")]),
+                dict(selector="th:hover",
+                     props=[("font-size", "10pt")]),
+                dict(selector="td",
+                     props=[('padding', "0em 0em")]),
+               # dict(selector="th:hover",
+               #      props=[("font-size", "12pt")]),
+                dict(selector="tr:hover td:hover",
+                     props=[('max-width', '200px'),
+                            ('font-weight', 'bold'),
+                            ('color', 'black'),
+                           ('font-size', '9pt')])
+    ]
+
+    cm =sns.diverging_palette(220,15,n=161)
+    def color_cell(val):
+        val = val[val.rindex(': ')+2:val.rindex('%')]
+        color = to_hex(cm[int(val)+30])
+        return 'background-color: %s' % color
+
+
+    styler = sorted_df.style
+    styler \
+        .applymap(color_cell) \
+        .set_properties(**{'max-width': '100px','font-size': '10pt', 'border-color': 'white'})\
+        .set_precision(2)\
+        .set_table_styles(magnify())
+    return styler
+
+def display_cluster_DVs(consensus, results):
+    nth = {
+        1: "first",
+        2: "second",
+        3: "third",
+        4: "fourth",
+        5: "fifth",
+        6: "sixth",
+        7: "seventh",
+        8: "eigth",
+        9: "ninth",
+        10: "tenth",
+        11: "11th",
+        12: "12th",
+        13: "13th",
+        14: "14th",
+        15: "15th",
+        16: "16th",
+        17: "17th",
+        18: "18th",
+        19: "19th",
+        20: "20th",
+    }
+    c = results.EFA.get_c()
+    cluster_DVs = results.HCA.get_cluster_DVs(inp='EFA%s_oblimin' % c)
+    df = consensus.get_consensus_cluster()['distance_df']
+    sorted_df = pd.DataFrame(data=np.zeros((len(df),20)), index=df.index)
+    for name, row in sorted_df.iterrows():
+        neighbors = [v for v in cluster_DVs.values() if name in format_variable_names(v)][0]
+        neighbors = format_variable_names(neighbors)
+        closest = 1-df.loc[name, neighbors].drop(name).sort_values()
+        closest = ['%s: %s%%' % (i,int(b*100)) for i,b in closest.iteritems()]
+        closest += ['' for _ in range(20-len(closest))]
+        sorted_df.loc[name] = closest
+
+    def magnify():
+        return [dict(selector="tr:hover",
+                    props=[("border-top", "2pt solid black"),
+                           ("border-bottom", "2pt solid black")]),
+                dict(selector="th:hover",
+                     props=[("font-size", "10pt")]),
+                dict(selector="td",
+                     props=[('padding', "0em 0em")]),
+               # dict(selector="th:hover",
+               #      props=[("font-size", "12pt")]),
+                dict(selector="tr:hover td:hover",
+                     props=[('max-width', '200px'),
+                            ('font-weight', 'bold'),
+                            ('color', 'black'),
+                           ('font-size', '9pt')])
+    ]
+
+    cm =sns.diverging_palette(220,15,n=161)
+    def color_cell(val):
+        if val =='':
+            return 'background-color: None'
+        num = val[val.rindex(': ')+2:val.rindex('%')]
+        color = to_hex(cm[int(num)+30])
+        return 'background-color: %s' % color
+
+
+    styler = sorted_df.style
+    styler \
+        .applymap(color_cell) \
+        .set_properties(**{'max-width': '100px',  'font-size': '10pt', 'border-color': 'white'})\
+        .set_precision(2)\
+        .set_table_styles(magnify())
+    return styler
